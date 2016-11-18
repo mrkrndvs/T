3a150bf729969aa9a65070013a8e0889eb2cf654
mrkrndvs <mrkrndvs@gmail.com>
2016-11-19 09:15:02 +1100

    Added 1 tweets

--- data/js/tweets/2016_11.js
+++ data/js/tweets/2016_11.js
@@ -1,6 +1,372 @@
 Grailbird.data.tweets_2016_11 = 
 [
 	{
+		"created_at": "Fri Nov 18 21:28:44 +0000 2016",
+		"id": 799726057649619000,
+		"id_str": "799726057649618945",
+		"text": "RT @GeoMouldey: Any SOLO using Technology teachers able to help? @gmacmanus @beechEdesignz #edchatnz https://t.co/0c4MhnzISH",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "edchatnz",
+					"indices": [
+						91,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GeoMouldey",
+					"name": "Steve Mouldey",
+					"id": 299908186,
+					"id_str": "299908186",
+					"indices": [
+						3,
+						14
+					]
+				},
+				{
+					"screen_name": "gmacmanus",
+					"name": "gmacmanus",
+					"id": 11175162,
+					"id_str": "11175162",
+					"indices": [
+						65,
+						75
+					]
+				},
+				{
+					"screen_name": "beechEdesignz",
+					"name": "Terry Beech",
+					"id": 550077657,
+					"id_str": "550077657",
+					"indices": [
+						76,
+						90
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/0c4MhnzISH",
+					"expanded_url": "https://twitter.com/CleoThorpeNgata/status/798985135110488064",
+					"display_url": "twitter.com/CleoThorpeNgat…",
+					"indices": [
+						101,
+						124
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "Educator interested in 21CL, ICT, Literacy and History. Actually, interested in learning and how together we are always better. Thoughts are my own.",
+			"url": "https://t.co/PkNuJdsUBx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PkNuJdsUBx",
+							"expanded_url": "http://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3581,
+			"friends_count": 2795,
+			"listed_count": 371,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4617,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24242,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Nov 18 21:28:23 +0000 2016",
+			"id": 799725966373175300,
+			"id_str": "799725966373175296",
+			"text": "Any SOLO using Technology teachers able to help? @gmacmanus @beechEdesignz #edchatnz https://t.co/0c4MhnzISH",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "edchatnz",
+						"indices": [
+							75,
+							84
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "gmacmanus",
+						"name": "gmacmanus",
+						"id": 11175162,
+						"id_str": "11175162",
+						"indices": [
+							49,
+							59
+						]
+					},
+					{
+						"screen_name": "beechEdesignz",
+						"name": "Terry Beech",
+						"id": 550077657,
+						"id_str": "550077657",
+						"indices": [
+							60,
+							74
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/0c4MhnzISH",
+						"expanded_url": "https://twitter.com/CleoThorpeNgata/status/798985135110488064",
+						"display_url": "twitter.com/CleoThorpeNgat…",
+						"indices": [
+							85,
+							108
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 299908186,
+				"id_str": "299908186",
+				"name": "Steve Mouldey",
+				"screen_name": "GeoMouldey",
+				"location": "Auckland, New Zealand",
+				"description": "Deputy Principal; curriculum hacker; design thinker; educational provocateur; Geographer; Google Certified Innovator; eFellow15",
+				"url": "https://t.co/Sp0WZuWBYZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Sp0WZuWBYZ",
+								"expanded_url": "http://www.stevemouldey.wordpress.com",
+								"display_url": "stevemouldey.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3998,
+				"friends_count": 1332,
+				"listed_count": 251,
+				"created_at": "Mon May 16 21:44:00 +0000 2011",
+				"favourites_count": 5672,
+				"utc_offset": 46800,
+				"time_zone": "Nuku'alofa",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 19430,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "709397",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/710030469/2cd976d9606b28d9bcb8992ada2a6557.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/710030469/2cd976d9606b28d9bcb8992ada2a6557.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/745093495996571649/CmlSSSM7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/745093495996571649/CmlSSSM7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/299908186/1352761067",
+				"profile_link_color": "FF3300",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "A0C5C7",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 798985135110488000,
+			"quoted_status_id_str": "798985135110488064",
+			"quoted_status": {
+				"created_at": "Wed Nov 16 20:24:35 +0000 2016",
+				"id": 798985135110488000,
+				"id_str": "798985135110488064",
+				"text": "Any NZ secondary level #technology teachers out there using #solotaxonomy ?",
+				"truncated": false,
+				"entities": {
+					"hashtags": [
+						{
+							"text": "technology",
+							"indices": [
+								23,
+								34
+							]
+						},
+						{
+							"text": "solotaxonomy",
+							"indices": [
+								60,
+								73
+							]
+						}
+					],
+					"symbols": [],
+					"user_mentions": [],
+					"urls": []
+				},
+				"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 1957516244,
+					"id_str": "1957516244",
+					"name": "Cleo Thorpe-Ngata",
+					"screen_name": "CleoThorpeNgata",
+					"location": "Gisborne, New Zealand",
+					"description": "passionate maker/artist/educator/learner",
+					"url": null,
+					"entities": {
+						"description": {
+							"urls": []
+						}
+					},
+					"protected": false,
+					"followers_count": 80,
+					"friends_count": 95,
+					"listed_count": 6,
+					"created_at": "Sat Oct 12 21:15:15 +0000 2013",
+					"favourites_count": 217,
+					"utc_offset": -28800,
+					"time_zone": "Pacific Time (US & Canada)",
+					"geo_enabled": false,
+					"verified": false,
+					"statuses_count": 215,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "C0DEED",
+					"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+					"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/677004863281209347/Yyo33dlI_normal.jpg",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/677004863281209347/Yyo33dlI_normal.jpg",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/1957516244/1450245564",
+					"profile_link_color": "1DA1F2",
+					"profile_sidebar_border_color": "C0DEED",
+					"profile_sidebar_fill_color": "DDEEF6",
+					"profile_text_color": "333333",
+					"profile_use_background_image": true,
+					"has_extended_profile": false,
+					"default_profile": true,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 2,
+				"favorite_count": 1,
+				"favorited": false,
+				"retweeted": false,
+				"lang": "en"
+			},
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 798985135110488000,
+		"quoted_status_id_str": "798985135110488064",
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Nov 18 20:47:54 +0000 2016",
 		"id": 799715779927121900,
 		"id_str": "799715779927121920",
