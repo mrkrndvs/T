688cc107ced54dfccf847149c05411240fbcbdd4
mrkrndvs <mrkrndvs@gmail.com>
2016-11-23 09:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24324,
-	"created_at": "Tue Nov 22 21:15:02 +0000 2016",
+	"tweets": 24325,
+	"created_at": "Tue Nov 22 22:15:02 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_11.js",
 		"year": 2016,
 		"var_name": "tweets_2016_11",
-		"tweet_count": 450,
+		"tweet_count": 451,
 		"month": 11
 	},
 	{
