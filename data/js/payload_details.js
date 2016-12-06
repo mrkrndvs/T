c2a578c5d5a9dda61715e5d54716e6607e30f9b4
mrkrndvs <mrkrndvs@gmail.com>
2016-12-06 23:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24595,
-	"created_at": "Tue Dec 06 11:14:59 +0000 2016",
+	"tweets": 24596,
+	"created_at": "Tue Dec 06 12:15:01 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_12.js
+++ data/js/tweets/2016_12.js
@@ -1,6 +1,380 @@
 Grailbird.data.tweets_2016_12 = 
 [
 	{
+		"created_at": "Tue Dec 06 11:57:41 +0000 2016",
+		"id": 806105327825391600,
+		"id_str": "806105327825391617",
+		"text": "RT @JoyKirr: Watching tonight! I love hearing student perspectives! \n#geniushour #studentvoice https://t.co/hQr6d9LSQQ",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "geniushour",
+					"indices": [
+						69,
+						80
+					]
+				},
+				{
+					"text": "studentvoice",
+					"indices": [
+						81,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JoyKirr",
+					"name": "Joy Kirr",
+					"id": 344350209,
+					"id_str": "344350209",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/hQr6d9LSQQ",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/805940838026969088",
+					"display_url": "twitter.com/mrkrndvs/statu…",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "Educator interested in 21CL, ICT, Literacy and History. Actually, interested in learning and how together we are always better. Thoughts are my own.",
+			"url": "https://t.co/PkNuJdsUBx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PkNuJdsUBx",
+							"expanded_url": "http://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3621,
+			"friends_count": 2811,
+			"listed_count": 377,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4617,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24582,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Dec 06 11:47:20 +0000 2016",
+			"id": 806102724945334300,
+			"id_str": "806102724945334272",
+			"text": "Watching tonight! I love hearing student perspectives! \n#geniushour #studentvoice https://t.co/hQr6d9LSQQ",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "geniushour",
+						"indices": [
+							56,
+							67
+						]
+					},
+					{
+						"text": "studentvoice",
+						"indices": [
+							68,
+							81
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/hQr6d9LSQQ",
+						"expanded_url": "https://twitter.com/mrkrndvs/status/805940838026969088",
+						"display_url": "twitter.com/mrkrndvs/statu…",
+						"indices": [
+							82,
+							105
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 344350209,
+				"id_str": "344350209",
+				"name": "Joy Kirr",
+				"screen_name": "JoyKirr",
+				"location": "Elk Grove, IL, AHSD25",
+				"description": "Optimism overcomes obstacles (Julie K.). Gr7 ELA NBCT, always trying to catch up to the students, while enjoying my home life to the fullest. #TTOG #GeniusHour",
+				"url": "https://t.co/yBAQNBxzbi",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/yBAQNBxzbi",
+								"expanded_url": "http://geniushour.blogspot.com",
+								"display_url": "geniushour.blogspot.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 13782,
+				"friends_count": 4840,
+				"listed_count": 853,
+				"created_at": "Thu Jul 28 22:55:23 +0000 2011",
+				"favourites_count": 3254,
+				"utc_offset": -21600,
+				"time_zone": "Central Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 70687,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBDFC1",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/667869012/41213525aa64f635ebaf338f2b916c95.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/667869012/41213525aa64f635ebaf338f2b916c95.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/793409571066826752/pOPPCM5X_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/793409571066826752/pOPPCM5X_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/344350209/1467226495",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 805940838026969100,
+			"quoted_status_id_str": "805940838026969088",
+			"quoted_status": {
+				"created_at": "Tue Dec 06 01:04:04 +0000 2016",
+				"id": 805940838026969100,
+				"id_str": "805940838026969088",
+				"text": "Edu On Air: Disrupting Learning - Student Perspectives https://t.co/rulOMYhchG via @anthsperanza #RenLead #GeniusHour",
+				"truncated": false,
+				"entities": {
+					"hashtags": [
+						{
+							"text": "RenLead",
+							"indices": [
+								97,
+								105
+							]
+						},
+						{
+							"text": "GeniusHour",
+							"indices": [
+								106,
+								117
+							]
+						}
+					],
+					"symbols": [],
+					"user_mentions": [
+						{
+							"screen_name": "anthsperanza",
+							"name": "Anthony Speranza",
+							"id": 130416490,
+							"id_str": "130416490",
+							"indices": [
+								83,
+								96
+							]
+						}
+					],
+					"urls": [
+						{
+							"url": "https://t.co/rulOMYhchG",
+							"expanded_url": "https://youtu.be/nuKqSU3OniA",
+							"display_url": "youtu.be/nuKqSU3OniA",
+							"indices": [
+								55,
+								78
+							]
+						}
+					]
+				},
+				"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 372768752,
+					"id_str": "372768752",
+					"name": "Aaron Davis",
+					"screen_name": "mrkrndvs",
+					"location": "Melbourne, Australia",
+					"description": "Educator interested in 21CL, ICT, Literacy and History. Actually, interested in learning and how together we are always better. Thoughts are my own.",
+					"url": "https://t.co/PkNuJdsUBx",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "https://t.co/PkNuJdsUBx",
+									"expanded_url": "http://readwriterespond.com",
+									"display_url": "readwriterespond.com",
+									"indices": [
+										0,
+										23
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": []
+						}
+					},
+					"protected": false,
+					"followers_count": 3621,
+					"friends_count": 2811,
+					"listed_count": 377,
+					"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+					"favourites_count": 4617,
+					"utc_offset": 36000,
+					"time_zone": "Brisbane",
+					"geo_enabled": false,
+					"verified": false,
+					"statuses_count": 24582,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "000000",
+					"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+					"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+					"profile_link_color": "FAB81E",
+					"profile_sidebar_border_color": "000000",
+					"profile_sidebar_fill_color": "000000",
+					"profile_text_color": "000000",
+					"profile_use_background_image": false,
+					"has_extended_profile": false,
+					"default_profile": false,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 0,
+				"favorite_count": 0,
+				"favorited": false,
+				"retweeted": false,
+				"possibly_sensitive": false,
+				"lang": "en"
+			},
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 805940838026969100,
+		"quoted_status_id_str": "805940838026969088",
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Dec 06 11:02:08 +0000 2016",
 		"id": 806091347371274200,
 		"id_str": "806091347371274240",
