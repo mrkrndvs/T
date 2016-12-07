0abbd5fe88cefea76c165f78258d83183cf724ee
mrkrndvs <mrkrndvs@gmail.com>
2016-12-07 13:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24596,
-	"created_at": "Tue Dec 06 12:15:01 +0000 2016",
+	"tweets": 24597,
+	"created_at": "Wed Dec 07 02:14:59 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweets/2016_12.js
+++ data/js/tweets/2016_12.js
@@ -1,6 +1,303 @@
 Grailbird.data.tweets_2016_12 = 
 [
 	{
+		"created_at": "Wed Dec 07 02:08:42 +0000 2016",
+		"id": 806319491864936400,
+		"id_str": "806319491864936449",
+		"text": "RT @JoyKirr: @whosanktheboat @mrkrndvs @anthsperanza Added Anthony's #EduOnAir here: https://t.co/pSZnjwro3P I LOVE \"trolling\" for #geniush…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "EduOnAir",
+					"indices": [
+						69,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JoyKirr",
+					"name": "Joy Kirr",
+					"id": 344350209,
+					"id_str": "344350209",
+					"indices": [
+						3,
+						11
+					]
+				},
+				{
+					"screen_name": "whosanktheboat",
+					"name": "Mr C ;",
+					"id": 2148930992,
+					"id_str": "2148930992",
+					"indices": [
+						13,
+						28
+					]
+				},
+				{
+					"screen_name": "mrkrndvs",
+					"name": "Aaron Davis",
+					"id": 372768752,
+					"id_str": "372768752",
+					"indices": [
+						29,
+						38
+					]
+				},
+				{
+					"screen_name": "anthsperanza",
+					"name": "Anthony Speranza",
+					"id": 130416490,
+					"id_str": "130416490",
+					"indices": [
+						39,
+						52
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/pSZnjwro3P",
+					"expanded_url": "http://www.livebinders.com/play/play/829279?tabid=3e23e619-a9a8-12cc-e879-861ce4b36b7c",
+					"display_url": "livebinders.com/play/play/8292…",
+					"indices": [
+						85,
+						108
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "Educator interested in 21CL, ICT, Literacy and History. Actually, interested in learning and how together we are always better. Thoughts are my own.",
+			"url": "https://t.co/PkNuJdsUBx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PkNuJdsUBx",
+							"expanded_url": "http://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3622,
+			"friends_count": 2811,
+			"listed_count": 377,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4617,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24583,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Dec 07 01:00:36 +0000 2016",
+			"id": 806302353783595000,
+			"id_str": "806302353783595008",
+			"text": "@whosanktheboat @mrkrndvs @anthsperanza Added Anthony's #EduOnAir here: https://t.co/pSZnjwro3P I LOVE \"trolling\" for #geniushour info! ;)",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "EduOnAir",
+						"indices": [
+							56,
+							65
+						]
+					},
+					{
+						"text": "geniushour",
+						"indices": [
+							118,
+							129
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "whosanktheboat",
+						"name": "Mr C ;",
+						"id": 2148930992,
+						"id_str": "2148930992",
+						"indices": [
+							0,
+							15
+						]
+					},
+					{
+						"screen_name": "mrkrndvs",
+						"name": "Aaron Davis",
+						"id": 372768752,
+						"id_str": "372768752",
+						"indices": [
+							16,
+							25
+						]
+					},
+					{
+						"screen_name": "anthsperanza",
+						"name": "Anthony Speranza",
+						"id": 130416490,
+						"id_str": "130416490",
+						"indices": [
+							26,
+							39
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/pSZnjwro3P",
+						"expanded_url": "http://www.livebinders.com/play/play/829279?tabid=3e23e619-a9a8-12cc-e879-861ce4b36b7c",
+						"display_url": "livebinders.com/play/play/8292…",
+						"indices": [
+							72,
+							95
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 806110658227880000,
+			"in_reply_to_status_id_str": "806110658227879937",
+			"in_reply_to_user_id": 2148930992,
+			"in_reply_to_user_id_str": "2148930992",
+			"in_reply_to_screen_name": "whosanktheboat",
+			"user": {
+				"id": 344350209,
+				"id_str": "344350209",
+				"name": "Joy Kirr",
+				"screen_name": "JoyKirr",
+				"location": "Elk Grove, IL, AHSD25",
+				"description": "Optimism overcomes obstacles (Julie K.). Gr7 ELA NBCT, always trying to catch up to the students, while enjoying my home life to the fullest. #TTOG #GeniusHour",
+				"url": "https://t.co/yBAQNBxzbi",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/yBAQNBxzbi",
+								"expanded_url": "http://geniushour.blogspot.com",
+								"display_url": "geniushour.blogspot.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 13788,
+				"friends_count": 4840,
+				"listed_count": 852,
+				"created_at": "Thu Jul 28 22:55:23 +0000 2011",
+				"favourites_count": 3254,
+				"utc_offset": -21600,
+				"time_zone": "Central Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 70689,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBDFC1",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/667869012/41213525aa64f635ebaf338f2b916c95.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/667869012/41213525aa64f635ebaf338f2b916c95.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/793409571066826752/pOPPCM5X_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/793409571066826752/pOPPCM5X_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/344350209/1467226495",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Dec 06 11:57:41 +0000 2016",
 		"id": 806105327825391600,
 		"id_str": "806105327825391617",
