36a3b1ff18cfc845c59c524a19aaad55fe8b61e6
mrkrndvs <mrkrndvs@gmail.com>
2016-12-28 04:15:16 +1100

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_12.js",
 		"year": 2016,
 		"var_name": "tweets_2016_12",
-		"tweet_count": 522,
+		"tweet_count": 523,
 		"month": 12
 	},
 	{
