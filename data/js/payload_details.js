0312b0c1b66556c2e854eb825ba46d73f3889cb6
mrkrndvs <mrkrndvs@gmail.com>
2017-02-14 12:15:03 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26027,
-	"created_at": "Mon Feb 13 22:15:02 +0000 2017",
+	"tweets": 26028,
+	"created_at": "Tue Feb 14 01:15:02 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_02.js
+++ data/js/tweets/2017_02.js
@@ -1,6 +1,277 @@
 Grailbird.data.tweets_2017_02 = 
 [
 	{
+		"created_at": "Tue Feb 14 00:22:45 +0000 2017",
+		"id": 831297593758011400,
+		"id_str": "831297593758011393",
+		"text": "RT @jayatwood: #sheetsgeeks beware:\"VLOOKUP is the gateway drug of spreadsheet formulas.\" @losersHQ #truth https://t.co/zMmjyJHWzR #edtecht…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sheetsgeeks",
+					"indices": [
+						15,
+						27
+					]
+				},
+				{
+					"text": "truth",
+					"indices": [
+						100,
+						106
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jayatwood",
+					"name": "Jay Atwood",
+					"id": 43616483,
+					"id_str": "43616483",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"screen_name": "losersHQ",
+					"name": "Coding is for Losers",
+					"id": 4472228655,
+					"id_str": "4472228655",
+					"indices": [
+						90,
+						99
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zMmjyJHWzR",
+					"expanded_url": "http://codingisforlosers.com",
+					"display_url": "codingisforlosers.com",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. Thoughts my own, not employers..",
+			"url": "https://t.co/PkNuJdsUBx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PkNuJdsUBx",
+							"expanded_url": "http://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3813,
+			"friends_count": 2864,
+			"listed_count": 393,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4641,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25992,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Feb 14 00:03:16 +0000 2017",
+			"id": 831292687605116900,
+			"id_str": "831292687605116928",
+			"text": "#sheetsgeeks beware:\"VLOOKUP is the gateway drug of spreadsheet formulas.\" @losersHQ #truth https://t.co/zMmjyJHWzR #edtechteam",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "sheetsgeeks",
+						"indices": [
+							0,
+							12
+						]
+					},
+					{
+						"text": "truth",
+						"indices": [
+							85,
+							91
+						]
+					},
+					{
+						"text": "edtechteam",
+						"indices": [
+							116,
+							127
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "losersHQ",
+						"name": "Coding is for Losers",
+						"id": 4472228655,
+						"id_str": "4472228655",
+						"indices": [
+							75,
+							84
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/zMmjyJHWzR",
+						"expanded_url": "http://codingisforlosers.com",
+						"display_url": "codingisforlosers.com",
+						"indices": [
+							92,
+							115
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 43616483,
+				"id_str": "43616483",
+				"name": "Jay Atwood",
+				"screen_name": "jayatwood",
+				"location": "Singapore",
+				"description": "Ameri-Aussie Ed Tech Geek, Google Certified Innovator & Trainer, ADE, former IB Psych Dep Chief Examiner, #travelnerd, photographer, problem solver",
+				"url": "http://t.co/s9FwYv2lL9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/s9FwYv2lL9",
+								"expanded_url": "http://google.com/+JayAtwood",
+								"display_url": "google.com/+JayAtwood",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4807,
+				"friends_count": 1387,
+				"listed_count": 257,
+				"created_at": "Sat May 30 23:39:21 +0000 2009",
+				"favourites_count": 511,
+				"utc_offset": 28800,
+				"time_zone": "Singapore",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3386,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/717306837004001280/KQZem7nt_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/717306837004001280/KQZem7nt_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/43616483/1361366212",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Feb 13 21:43:56 +0000 2017",
 		"id": 831257623424929800,
 		"id_str": "831257623424929793",
