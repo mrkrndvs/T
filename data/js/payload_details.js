43c0fcf1ece1bfdadb7dd3ab0ee2453caeeed593
mrkrndvs <mrkrndvs@gmail.com>
2017-02-28 03:15:11 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26260,
-	"created_at": "Mon Feb 27 11:15:05 +0000 2017",
+	"tweets": 26261,
+	"created_at": "Mon Feb 27 16:15:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_02.js",
 		"year": 2017,
 		"var_name": "tweets_2017_02",
-		"tweet_count": 516,
+		"tweet_count": 517,
 		"month": 2
 	},
 	{
