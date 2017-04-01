84f716ce40bbaf70d8880cfaa72b6cd6e7c53fa1
mrkrndvs <mrkrndvs@gmail.com>
2017-04-02 09:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26606,
-	"created_at": "Sat Apr 01 17:14:58 +0000 2017",
+	"tweets": 26607,
+	"created_at": "Sat Apr 01 23:14:58 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_04.js
+++ data/js/tweets/2017_04.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Sat Apr 01 22:17:52 +0000 2017",
+		"id": 848298393021456400,
+		"id_str": "848298393021456384",
+		"text": "RT @hbailie: Recommended read on @Medium: “The Pros and Cons of Twitter’s New Rules for Replies” https://t.co/5YG0nN7Ck0",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hbailie",
+					"name": "Heather Bailie",
+					"id": 16589509,
+					"id_str": "16589509",
+					"indices": [
+						3,
+						11
+					]
+				},
+				{
+					"screen_name": "Medium",
+					"name": "Medium",
+					"id": 571202103,
+					"id_str": "571202103",
+					"indices": [
+						33,
+						40
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5YG0nN7Ck0",
+					"expanded_url": "http://ift.tt/2nkOu1W",
+					"display_url": "ift.tt/2nkOu1W",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. Thoughts my own, not employers..",
+			"url": "https://t.co/PkNuJdsUBx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PkNuJdsUBx",
+							"expanded_url": "http://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3923,
+			"friends_count": 2902,
+			"listed_count": 396,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4673,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 26553,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Apr 01 22:12:35 +0000 2017",
+			"id": 848297063636971500,
+			"id_str": "848297063636971522",
+			"text": "Recommended read on @Medium: “The Pros and Cons of Twitter’s New Rules for Replies” https://t.co/5YG0nN7Ck0",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Medium",
+						"name": "Medium",
+						"id": 571202103,
+						"id_str": "571202103",
+						"indices": [
+							20,
+							27
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/5YG0nN7Ck0",
+						"expanded_url": "http://ift.tt/2nkOu1W",
+						"display_url": "ift.tt/2nkOu1W",
+						"indices": [
+							84,
+							107
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16589509,
+				"id_str": "16589509",
+				"name": "Heather Bailie",
+				"screen_name": "hbailie",
+				"location": "Melbourne, Australia",
+				"description": "Learner, teacher, teacher-librarian, ICT geek. MEd (KNDI) CSU student, GCI #GTASyd, #curation @ https://t.co/LeJUESgtId",
+				"url": "http://t.co/iQ7th93loI",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/iQ7th93loI",
+								"expanded_url": "http://www.hbailie.com/",
+								"display_url": "hbailie.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/LeJUESgtId",
+								"expanded_url": "http://Pearltrees.com/hbailie",
+								"display_url": "Pearltrees.com/hbailie",
+								"indices": [
+									96,
+									119
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 2951,
+				"friends_count": 1837,
+				"listed_count": 313,
+				"created_at": "Sat Oct 04 07:51:06 +0000 2008",
+				"favourites_count": 605,
+				"utc_offset": 36000,
+				"time_zone": "Melbourne",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17456,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "352726",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/3254009/P7210138.JPG",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/3254009/P7210138.JPG",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/715151357104295936/BaUNXbOK_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/715151357104295936/BaUNXbOK_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16589509/1442387274",
+				"profile_link_color": "86030C",
+				"profile_sidebar_border_color": "829D5E",
+				"profile_sidebar_fill_color": "E6810F",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Apr 01 17:06:27 +0000 2017",
 		"id": 848220022254309400,
 		"id_str": "848220022254309376",
