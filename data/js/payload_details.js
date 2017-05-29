5e3245fd3fe45d1a4a8f7851de487b6bb4305ab5
mrkrndvs <mrkrndvs@gmail.com>
2017-05-29 14:15:07 +1000

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27671,
-	"created_at": "Mon May 29 03:15:06 +0000 2017",
+	"tweets": 27675,
+	"created_at": "Mon May 29 04:15:06 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_05.js",
 		"year": 2017,
 		"var_name": "tweets_2017_05",
-		"tweet_count": 535,
+		"tweet_count": 539,
 		"month": 5
 	},
 	{
