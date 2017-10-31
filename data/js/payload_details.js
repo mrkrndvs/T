0b89454c4caae36a37a19dcc060e1a7566c606f5
Aaron Davis <mrkrndvs@gmail.com>
2017-10-31 21:15:03 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29548,
-	"created_at": "Tue Oct 31 08:15:03 +0000 2017",
+	"tweets": 29549,
+	"created_at": "Tue Oct 31 10:15:02 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_10.js
+++ data/js/tweets/2017_10.js
@@ -1,6 +1,298 @@
 Grailbird.data.tweets_2017_10 = 
 [
 	{
+		"created_at": "Tue Oct 31 09:58:21 +0000 2017",
+		"id": 925300902881214500,
+		"id_str": "925300902881214465",
+		"text": "RT @edublogs: @rmbyrne @Larryferlazzo @mrkrndvs @dogtrax we mentioned your posts in 'Blogging Around the Web'",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "edublogs",
+					"name": "edublogs",
+					"id": 14091545,
+					"id_str": "14091545",
+					"indices": [
+						3,
+						12
+					]
+				},
+				{
+					"screen_name": "rmbyrne",
+					"name": "Richard Byrne",
+					"id": 11112092,
+					"id_str": "11112092",
+					"indices": [
+						14,
+						22
+					]
+				},
+				{
+					"screen_name": "Larryferlazzo",
+					"name": "Larry Ferlazzo",
+					"id": 18107749,
+					"id_str": "18107749",
+					"indices": [
+						23,
+						37
+					]
+				},
+				{
+					"screen_name": "mrkrndvs",
+					"name": "Aaron Davis 🏘️",
+					"id": 372768752,
+					"id_str": "372768752",
+					"indices": [
+						38,
+						47
+					]
+				},
+				{
+					"screen_name": "dogtrax",
+					"name": "KevinHodgson",
+					"id": 13307352,
+					"id_str": "13307352",
+					"indices": [
+						48,
+						56
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. I write at https://t.co/PkNuJdsUBx",
+			"url": "https://t.co/uTlsCeAwJ7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PkNuJdsUBx",
+							"expanded_url": "http://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								136,
+								159
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4293,
+			"friends_count": 3009,
+			"listed_count": 396,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4871,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 29403,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Oct 31 09:39:48 +0000 2017",
+			"id": 925296234201473000,
+			"id_str": "925296234201473024",
+			"text": "@rmbyrne @Larryferlazzo @mrkrndvs @dogtrax we mentioned your posts in 'Blogging Around the Web'",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "rmbyrne",
+						"name": "Richard Byrne",
+						"id": 11112092,
+						"id_str": "11112092",
+						"indices": [
+							0,
+							8
+						]
+					},
+					{
+						"screen_name": "Larryferlazzo",
+						"name": "Larry Ferlazzo",
+						"id": 18107749,
+						"id_str": "18107749",
+						"indices": [
+							9,
+							23
+						]
+					},
+					{
+						"screen_name": "mrkrndvs",
+						"name": "Aaron Davis 🏘️",
+						"id": 372768752,
+						"id_str": "372768752",
+						"indices": [
+							24,
+							33
+						]
+					},
+					{
+						"screen_name": "dogtrax",
+						"name": "KevinHodgson",
+						"id": 13307352,
+						"id_str": "13307352",
+						"indices": [
+							34,
+							42
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 925294403622539300,
+			"in_reply_to_status_id_str": "925294403622539265",
+			"in_reply_to_user_id": 14091545,
+			"in_reply_to_user_id_str": "14091545",
+			"in_reply_to_screen_name": "edublogs",
+			"user": {
+				"id": 14091545,
+				"id_str": "14091545",
+				"name": "edublogs",
+				"screen_name": "edublogs",
+				"location": "Australia",
+				"description": "We are the easiest, safest and most powerful educational blogging platform with over 4 million blogs and counting! Check out https://t.co/V5oDMNUCQB too.",
+				"url": "http://t.co/5jEa8hIjgx",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/5jEa8hIjgx",
+								"expanded_url": "http://edublogs.org",
+								"display_url": "edublogs.org",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/V5oDMNUCQB",
+								"expanded_url": "http://CampusPress.com",
+								"display_url": "CampusPress.com",
+								"indices": [
+									125,
+									148
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 41034,
+				"friends_count": 8860,
+				"listed_count": 1558,
+				"created_at": "Thu Mar 06 21:39:49 +0000 2008",
+				"favourites_count": 497,
+				"utc_offset": 39600,
+				"time_zone": "Melbourne",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 10806,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B4E9F3",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/130366107/edu_tweet5.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/130366107/edu_tweet5.jpg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/378800000740807023/3bea1fd759cd71106768e9e97619aa55_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000740807023/3bea1fd759cd71106768e9e97619aa55_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14091545/1504059340",
+				"profile_link_color": "00C8FF",
+				"profile_sidebar_border_color": "AFE1EC",
+				"profile_sidebar_fill_color": "E3F7FB",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Oct 31 07:57:54 +0000 2017",
 		"id": 925270592688865300,
 		"id_str": "925270592688865280",
@@ -82,7 +374,7 @@ Grailbird.data.tweets_2017_10 =
 				},
 				{
 					"screen_name": "MrAdrianQuick",
-					"name": "Adrian Quick 🏳️‍🌈",
+					"name": "Adrian Quick 🏳️🌈",
 					"id": 873086655577473000,
 					"id_str": "873086655577473024",
 					"indices": [
