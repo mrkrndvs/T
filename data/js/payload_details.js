950d8eaf667ffc84040edb281582c6d6102b56c9
Aaron Davis <mrkrndvs@gmail.com>
2017-11-05 09:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29582,
-	"created_at": "Sat Nov 04 10:14:59 +0000 2017",
+	"tweets": 29583,
+	"created_at": "Sat Nov 04 22:15:01 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,372 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Sat Nov 04 21:50:39 +0000 2017",
+		"id": 926929710130413600,
+		"id_str": "926929710130413569",
+		"text": "RT @MayneReport: Good to see no full page gambling ads in either The Sunday Age or The Sunday Herald Sun today. https://t.co/pTCYiFlozE",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MayneReport",
+					"name": "Stephen Mayne",
+					"id": 33811563,
+					"id_str": "33811563",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 926920498943991800,
+					"id_str": "926920498943991808",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+					"url": "https://t.co/pTCYiFlozE",
+					"display_url": "pic.twitter.com/pTCYiFlozE",
+					"expanded_url": "https://twitter.com/MayneReport/status/926920500915322880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 926920500915322900,
+					"source_status_id_str": "926920500915322880",
+					"source_user_id": 33811563,
+					"source_user_id_str": "33811563"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 926920498943991800,
+					"id_str": "926920498943991808",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+					"url": "https://t.co/pTCYiFlozE",
+					"display_url": "pic.twitter.com/pTCYiFlozE",
+					"expanded_url": "https://twitter.com/MayneReport/status/926920500915322880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 926920500915322900,
+					"source_status_id_str": "926920500915322880",
+					"source_user_id": 33811563,
+					"source_user_id_str": "33811563"
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. I write at https://t.co/PkNuJdsUBx",
+			"url": "https://t.co/uTlsCeAwJ7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PkNuJdsUBx",
+							"expanded_url": "http://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								136,
+								159
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4302,
+			"friends_count": 3008,
+			"listed_count": 396,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4877,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 29437,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Nov 04 21:14:03 +0000 2017",
+			"id": 926920500915322900,
+			"id_str": "926920500915322880",
+			"text": "Good to see no full page gambling ads in either The Sunday Age or The Sunday Herald Sun today. https://t.co/pTCYiFlozE",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 926920498943991800,
+						"id_str": "926920498943991808",
+						"indices": [
+							95,
+							118
+						],
+						"media_url": "http://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+						"url": "https://t.co/pTCYiFlozE",
+						"display_url": "pic.twitter.com/pTCYiFlozE",
+						"expanded_url": "https://twitter.com/MayneReport/status/926920500915322880/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 926920498943991800,
+						"id_str": "926920498943991808",
+						"indices": [
+							95,
+							118
+						],
+						"media_url": "http://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DN0VSGKVoAAbOf8.jpg",
+						"url": "https://t.co/pTCYiFlozE",
+						"display_url": "pic.twitter.com/pTCYiFlozE",
+						"expanded_url": "https://twitter.com/MayneReport/status/926920500915322880/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 33811563,
+				"id_str": "33811563",
+				"name": "Stephen Mayne",
+				"screen_name": "MayneReport",
+				"location": "Melbourne",
+				"description": "Many hats # Comms adviser, Alliance for Gambling Reform # Contributor, The Constant Investor # Crikey founder/contributor # ASA director # Ex City of Melb Cr",
+				"url": "https://t.co/IX8KpF7g0y",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/IX8KpF7g0y",
+								"expanded_url": "http://www.pokiesplayyou.org.au/",
+								"display_url": "pokiesplayyou.org.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 29116,
+				"friends_count": 7477,
+				"listed_count": 700,
+				"created_at": "Tue Apr 21 04:28:55 +0000 2009",
+				"favourites_count": 39,
+				"utc_offset": 39600,
+				"time_zone": "Melbourne",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 57061,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EDECE9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1099402551/q702572950_4341_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099402551/q702572950_4341_normal.jpg",
+				"profile_link_color": "088253",
+				"profile_sidebar_border_color": "D3D2CF",
+				"profile_sidebar_fill_color": "E3E2DE",
+				"profile_text_color": "634047",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Nov 04 09:42:21 +0000 2017",
 		"id": 926746427593814000,
 		"id_str": "926746427593814016",
