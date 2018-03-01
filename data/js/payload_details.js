714e43b8bdf492012289bd0f3335f9b8d46f7518
Aaron Davis <mrkrndvs@gmail.com>
2018-03-01 21:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30518,
-	"created_at": "Thu Mar 01 06:14:59 +0000 2018",
+	"tweets": 30519,
+	"created_at": "Thu Mar 01 10:14:59 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Thu Mar 01 09:31:31 +0000 2018",
+		"id": 969143084318564400,
+		"id_str": "969143084318564352",
+		"text": "@adriancamm @DLTVictoria @samjirwin @CorrieB @GeelongGrammar @GreaterGeelong @Ben_Mr_G @FifiStafford @fionaf_fiona… https://t.co/5qK3LjxIHZ",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "adriancamm",
+					"name": "Adrian Camm",
+					"id": 17730865,
+					"id_str": "17730865",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "DLTVictoria",
+					"name": "DLTV",
+					"id": 16281844,
+					"id_str": "16281844",
+					"indices": [
+						12,
+						24
+					]
+				},
+				{
+					"screen_name": "samjirwin",
+					"name": "Sam Irwin",
+					"id": 198727323,
+					"id_str": "198727323",
+					"indices": [
+						25,
+						35
+					]
+				},
+				{
+					"screen_name": "CorrieB",
+					"name": "Corrie Barclay",
+					"id": 196529471,
+					"id_str": "196529471",
+					"indices": [
+						36,
+						44
+					]
+				},
+				{
+					"screen_name": "GeelongGrammar",
+					"name": "Geelong Grammar",
+					"id": 929019866,
+					"id_str": "929019866",
+					"indices": [
+						45,
+						60
+					]
+				},
+				{
+					"screen_name": "GreaterGeelong",
+					"name": "CityofGreaterGeelong",
+					"id": 171801336,
+					"id_str": "171801336",
+					"indices": [
+						61,
+						76
+					]
+				},
+				{
+					"screen_name": "Ben_Mr_G",
+					"name": "Ben Gallagher",
+					"id": 38384508,
+					"id_str": "38384508",
+					"indices": [
+						77,
+						86
+					]
+				},
+				{
+					"screen_name": "FifiStafford",
+					"name": "Fiona Stafford",
+					"id": 2778315966,
+					"id_str": "2778315966",
+					"indices": [
+						87,
+						100
+					]
+				},
+				{
+					"screen_name": "fionaf_fiona",
+					"name": "Fiona Fitzgerald",
+					"id": 2470172401,
+					"id_str": "2470172401",
+					"indices": [
+						101,
+						114
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5qK3LjxIHZ",
+					"expanded_url": "https://twitter.com/i/web/status/969143084318564352",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 968973350390218800,
+		"in_reply_to_status_id_str": "968973350390218752",
+		"in_reply_to_user_id": 17730865,
+		"in_reply_to_user_id_str": "17730865",
+		"in_reply_to_screen_name": "adriancamm",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4271,
+			"friends_count": 1115,
+			"listed_count": 395,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4953,
+			"utc_offset": 36000,
+			"time_zone": "Brisbane",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30335,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Mar 01 05:23:26 +0000 2018",
 		"id": 969080649410863100,
 		"id_str": "969080649410863104",
