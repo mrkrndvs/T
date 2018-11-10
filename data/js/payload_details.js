0ba7522dd6ac3ab9c3d5a306fd82af495bb2c0c0
Aaron Davis <mrkrndvs@gmail.com>
2018-11-10 22:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31299,
-	"created_at": "Sat Nov 10 04:14:58 +0000 2018",
+	"tweets": 31300,
+	"created_at": "Sat Nov 10 11:14:58 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sat Nov 10 10:27:37 +0000 2018",
+		"id": 1061203717741920300,
+		"id_str": "1061203717741920257",
+		"full_text": "@seanoneal @vulture Your comments about mediocrity of music reminds me of @jackantonoff reflections on Don't Take the Money, when after unpacking the song in 8 minutes he then bursts the bubble by highlighting the banality of it all, incl missing birthday parties (https://t.co/MzWlyInDzD)",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			289
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "seanoneal",
+					"name": "Sean O'Neal",
+					"id": 22805603,
+					"id_str": "22805603",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "vulture",
+					"name": "Vulture",
+					"id": 24216951,
+					"id_str": "24216951",
+					"indices": [
+						11,
+						19
+					]
+				},
+				{
+					"screen_name": "jackantonoff",
+					"name": "jackantonoff",
+					"id": 414569910,
+					"id_str": "414569910",
+					"indices": [
+						74,
+						87
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MzWlyInDzD",
+					"expanded_url": "https://collect.readwriterespond.com/in-defense-of-the-mediocre-music-biopic/",
+					"display_url": "collect.readwriterespond.com/in-defense-of-…",
+					"indices": [
+						265,
+						288
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1059899410837069800,
+		"in_reply_to_status_id_str": "1059899410837069824",
+		"in_reply_to_user_id": 22805603,
+		"in_reply_to_user_id_str": "22805603",
+		"in_reply_to_screen_name": "seanoneal",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4284,
+			"friends_count": 1109,
+			"listed_count": 382,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4837,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30962,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@seanoneal @vulture Your comments about mediocrity of music reminds me of @jackantonoff reflections on Don't Take the Money, when after unpacking the song in 8 minutes he then bursts the bubble by highlighting the banality of it all, incl missing birthday parties (https://t.co/MzWlyInDzD)"
+	},
+	{
 		"created_at": "Sat Nov 10 03:42:35 +0000 2018",
 		"id": 1061101787426500600,
 		"id_str": "1061101787426500608",
