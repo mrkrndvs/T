7ed542f455e15ac6153b94ed3004f873ed72455f
Aaron Davis <mrkrndvs@gmail.com>
2018-11-13 22:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31306,
-	"created_at": "Mon Nov 12 21:14:59 +0000 2018",
+	"tweets": 31307,
+	"created_at": "Tue Nov 13 11:14:59 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Tue Nov 13 10:46:40 +0000 2018",
+		"id": 1062295677823156200,
+		"id_str": "1062295677823156224",
+		"full_text": "@TIDEpodcast In response to @daibarnes and @dajbelshaw conversation, am wondering whether both @jamesbridle and @tristanharris each serve a particular purpose in becoming more informed? Elaborated here https://t.co/2MIhRYj7ed #EdTech",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			233
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "EdTech",
+					"indices": [
+						226,
+						233
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TIDEpodcast",
+					"name": "TIDE podcast",
+					"id": 4857178299,
+					"id_str": "4857178299",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "daibarnes",
+					"name": "Dai Barnes 👣☀️🌧️",
+					"id": 14143403,
+					"id_str": "14143403",
+					"indices": [
+						28,
+						38
+					]
+				},
+				{
+					"screen_name": "dajbelshaw",
+					"name": "Doug Belshaw | mastodon.social/@dajbelshaw",
+					"id": 764365,
+					"id_str": "764365",
+					"indices": [
+						43,
+						54
+					]
+				},
+				{
+					"screen_name": "jamesbridle",
+					"name": "James Bridle",
+					"id": 187512368,
+					"id_str": "187512368",
+					"indices": [
+						95,
+						107
+					]
+				},
+				{
+					"screen_name": "tristanharris",
+					"name": "Tristan Harris",
+					"id": 5854882,
+					"id_str": "5854882",
+					"indices": [
+						112,
+						126
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2MIhRYj7ed",
+					"expanded_url": "https://collect.readwriterespond.com/tide-112/",
+					"display_url": "collect.readwriterespond.com/tide-112/",
+					"indices": [
+						202,
+						225
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1060282080872017900,
+		"in_reply_to_status_id_str": "1060282080872017920",
+		"in_reply_to_user_id": 4857178299,
+		"in_reply_to_user_id_str": "4857178299",
+		"in_reply_to_screen_name": "TIDEpodcast",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4286,
+			"friends_count": 1109,
+			"listed_count": 382,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4837,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30969,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@TIDEpodcast In response to @daibarnes and @dajbelshaw conversation, am wondering whether both @jamesbridle and @tristanharris each serve a particular purpose in becoming more informed? Elaborated here https://t.co/2MIhRYj7ed #EdTech"
+	},
+	{
 		"created_at": "Mon Nov 12 20:31:50 +0000 2018",
 		"id": 1062080550683508700,
 		"id_str": "1062080550683508739",
