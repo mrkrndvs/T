07126f802e5700935805e04ae4fe456a70e400d4
Aaron Davis <mrkrndvs@gmail.com>
2019-01-06 21:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31340,
-	"created_at": "Sun Dec 30 03:14:58 +0000 2018",
+	"tweets": 31341,
+	"created_at": "Sun Jan 06 10:14:58 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_01.js
@@ -0,0 +1,180 @@
+Grailbird.data.tweets_2019_01 = 
+[
+	{
+		"created_at": "Sun Jan 06 09:53:47 +0000 2019",
+		"id": 1081851314911567900,
+		"id_str": "1081851314911567873",
+		"full_text": "@kathleen_morris @misskyritsis @lindayollis @MrHairPhysEd @robkellytweets @Lv2teachNtravel I have experienced a combination of LMS (Sentral, Compass, Synergetic) and social media (Facebook pages). I have discussed the use of Slides for newsletters in the past. I also think that there is scope for storing ‘newsletters’ in Google Drive to embed… https://t.co/eezAIc4mIg",
+		"truncated": false,
+		"display_text_range": [
+			91,
+			369
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kathleen_morris",
+					"name": "Kathleen Morris",
+					"id": 23413264,
+					"id_str": "23413264",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "misskyritsis",
+					"name": "Eleni Kyritsis",
+					"id": 1461879277,
+					"id_str": "1461879277",
+					"indices": [
+						17,
+						30
+					]
+				},
+				{
+					"screen_name": "lindayollis",
+					"name": "Linda Yollis",
+					"id": 142013481,
+					"id_str": "142013481",
+					"indices": [
+						31,
+						43
+					]
+				},
+				{
+					"screen_name": "MrHairPhysEd",
+					"name": "Andy Hair",
+					"id": 59393063,
+					"id_str": "59393063",
+					"indices": [
+						44,
+						57
+					]
+				},
+				{
+					"screen_name": "robkellytweets",
+					"name": "Rob Kelly (B.Ed)",
+					"id": 48339997,
+					"id_str": "48339997",
+					"indices": [
+						58,
+						73
+					]
+				},
+				{
+					"screen_name": "Lv2teachNtravel",
+					"name": "Marion Ellery (Mazz)",
+					"id": 2802658441,
+					"id_str": "2802658441",
+					"indices": [
+						74,
+						90
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/eezAIc4mIg",
+					"expanded_url": "https://collect.readwriterespond.com/kathleen-morris-on-communication/",
+					"display_url": "collect.readwriterespond.com/kathleen-morri…",
+					"indices": [
+						346,
+						369
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": 1080932521385193500,
+		"in_reply_to_status_id_str": "1080932521385193472",
+		"in_reply_to_user_id": 23413264,
+		"in_reply_to_user_id_str": "23413264",
+		"in_reply_to_screen_name": "kathleen_morris",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4271,
+			"friends_count": 987,
+			"listed_count": 380,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4838,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30999,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@kathleen_morris @misskyritsis @lindayollis @MrHairPhysEd @robkellytweets @Lv2teachNtravel I have experienced a combination of LMS (Sentral, Compass, Synergetic) and social media (Facebook pages). I have discussed the use of Slides for newsletters in the past. I also think that there is scope for storing ‘newsletters’ in Google Drive to embed… https://t.co/eezAIc4mIg"
+	}
+]
