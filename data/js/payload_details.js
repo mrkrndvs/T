e5ba3ca7c3235b889a2e8ed26c0f8362d9fc61b6
Aaron Davis <mrkrndvs@gmail.com>
2019-02-01 22:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31365,
-	"created_at": "Fri Feb 01 10:14:59 +0000 2019",
+	"tweets": 31366,
+	"created_at": "Fri Feb 01 11:14:59 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,304 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 01 10:20:06 +0000 2019",
+		"id": 1091280021598089200,
+		"id_str": "1091280021598089218",
+		"full_text": "@stringer_andrea @7Mrsjames @gripgirl @BrendaQuayle1 @mesterman @markliddell @aliceleung @JonTzantzaris @HelenMorgani @TESOLoz https://t.co/r8eZhn3jWF",
+		"truncated": false,
+		"display_text_range": [
+			126,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stringer_andrea",
+					"name": "Andrea Stringer 🇦🇺",
+					"id": 585586447,
+					"id_str": "585586447",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "7Mrsjames",
+					"name": "JJ (Jeannette James)",
+					"id": 337381294,
+					"id_str": "337381294",
+					"indices": [
+						17,
+						27
+					]
+				},
+				{
+					"screen_name": "gripgirl",
+					"name": "Anne Gripton",
+					"id": 21369142,
+					"id_str": "21369142",
+					"indices": [
+						28,
+						37
+					]
+				},
+				{
+					"screen_name": "BrendaQuayle1",
+					"name": "Brenda Quayle",
+					"id": 597102512,
+					"id_str": "597102512",
+					"indices": [
+						38,
+						52
+					]
+				},
+				{
+					"screen_name": "mesterman",
+					"name": "Matt Esterman",
+					"id": 14906194,
+					"id_str": "14906194",
+					"indices": [
+						53,
+						63
+					]
+				},
+				{
+					"screen_name": "markliddell",
+					"name": "Mark Liddell",
+					"id": 19372896,
+					"id_str": "19372896",
+					"indices": [
+						64,
+						76
+					]
+				},
+				{
+					"screen_name": "aliceleung",
+					"name": "Alice Leung",
+					"id": 31530480,
+					"id_str": "31530480",
+					"indices": [
+						77,
+						88
+					]
+				},
+				{
+					"screen_name": "JonTzantzaris",
+					"name": "John Tzantzaris",
+					"id": 65995140,
+					"id_str": "65995140",
+					"indices": [
+						89,
+						103
+					]
+				},
+				{
+					"screen_name": "HelenMorgani",
+					"name": "Helen Morgani",
+					"id": 2816090648,
+					"id_str": "2816090648",
+					"indices": [
+						104,
+						117
+					]
+				},
+				{
+					"screen_name": "TESOLoz",
+					"name": "Cindy Valdez-Adams",
+					"id": 1320024973,
+					"id_str": "1320024973",
+					"indices": [
+						118,
+						126
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1091280012370538500,
+					"id_str": "1091280012370538497",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DyUBY6_UcAE7AL4.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DyUBY6_UcAE7AL4.jpg",
+					"url": "https://t.co/r8eZhn3jWF",
+					"display_url": "pic.twitter.com/r8eZhn3jWF",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1091280021598089218/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 278,
+							"h": 174,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 278,
+							"h": 174,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 278,
+							"h": 174,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1091280012370538500,
+					"id_str": "1091280012370538497",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DyUBY6_UcAE7AL4.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DyUBY6_UcAE7AL4.jpg",
+					"url": "https://t.co/r8eZhn3jWF",
+					"display_url": "pic.twitter.com/r8eZhn3jWF",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1091280021598089218/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 278,
+							"h": 174,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 278,
+							"h": 174,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 278,
+							"h": 174,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							139,
+							87
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DyUBY6_UcAE7AL4.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1091229022640427000,
+		"in_reply_to_status_id_str": "1091229022640427008",
+		"in_reply_to_user_id": 585586447,
+		"in_reply_to_user_id_str": "585586447",
+		"in_reply_to_screen_name": "stringer_andrea",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4268,
+			"friends_count": 988,
+			"listed_count": 378,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4838,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27818,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@stringer_andrea @7Mrsjames @gripgirl @BrendaQuayle1 @mesterman @markliddell @aliceleung @JonTzantzaris @HelenMorgani @TESOLoz https://t.co/r8eZhn3jWF"
+	},
+	{
 		"created_at": "Fri Feb 01 09:33:34 +0000 2019",
 		"id": 1091268311206092800,
 		"id_str": "1091268311206092801",
