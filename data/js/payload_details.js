4cda30bcb937fe854385f10b35c23fe0d3ea7853
Aaron Davis <mrkrndvs@gmail.com>
2019-02-21 14:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31373,
-	"created_at": "Tue Feb 19 13:14:58 +0000 2019",
+	"tweets": 31374,
+	"created_at": "Thu Feb 21 03:14:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Thu Feb 21 02:36:22 +0000 2019",
+		"id": 1098411074427138000,
+		"id_str": "1098411074427138048",
+		"full_text": "@DrSRiddle Reminds me of the planning document in Secret Life of Pets https://t.co/qXTCP2KeWC",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			69
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DrSRiddle",
+					"name": "Stewart Riddle",
+					"id": 1315624051,
+					"id_str": "1315624051",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1098411055137534000,
+					"id_str": "1098411055137533952",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dz5XCPbVsAAKXrS.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dz5XCPbVsAAKXrS.jpg",
+					"url": "https://t.co/qXTCP2KeWC",
+					"display_url": "pic.twitter.com/qXTCP2KeWC",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1098411074427138048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 330,
+							"h": 168,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 330,
+							"h": 168,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 330,
+							"h": 168,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1098411055137534000,
+					"id_str": "1098411055137533952",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dz5XCPbVsAAKXrS.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dz5XCPbVsAAKXrS.jpg",
+					"url": "https://t.co/qXTCP2KeWC",
+					"display_url": "pic.twitter.com/qXTCP2KeWC",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1098411074427138048/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 330,
+							"h": 168,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 330,
+							"h": 168,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 330,
+							"h": 168,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							55,
+							28
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Dz5XCPbVsAAKXrS.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1098399236511559700,
+		"in_reply_to_status_id_str": "1098399236511559680",
+		"in_reply_to_user_id": 1315624051,
+		"in_reply_to_user_id_str": "1315624051",
+		"in_reply_to_screen_name": "DrSRiddle",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4269,
+			"friends_count": 989,
+			"listed_count": 379,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4839,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27802,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@DrSRiddle Reminds me of the planning document in Secret Life of Pets https://t.co/qXTCP2KeWC"
+	},
+	{
 		"created_at": "Tue Feb 19 12:41:26 +0000 2019",
 		"id": 1097838568536457200,
 		"id_str": "1097838568536457216",
