39ba10942f4a16906bf67426439a721c567fc72b
Aaron Davis <mrkrndvs@gmail.com>
2019-06-21 07:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31462,
-	"created_at": "Wed Jun 19 13:14:59 +0000 2019",
+	"tweets": 31463,
+	"created_at": "Thu Jun 20 21:14:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Thu Jun 20 21:14:06 +0000 2019",
+		"id": 1141178331640893400,
+		"id_str": "1141816516553449477",
+		"full_text": "Jaidyn Stephenson has been handed a 22-match ban (12 matches suspended) and fined $20,000. I'm disappointed as I'd backed him to get seven weeks through the AFL's official wagering partner.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			189
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 474493404,
+			"id_str": "474493404",
+			"name": "Titus O'Reily",
+			"screen_name": "TitusOReily",
+			"location": "Melbourne",
+			"description": "Titus is known as ‘the voice of Australia’s working class’, despite the fact he long lost touch with them and in the main, reality.",
+			"url": "http://t.co/JTIs5PnNGJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/JTIs5PnNGJ",
+							"expanded_url": "http://www.titusoreily.com/",
+							"display_url": "titusoreily.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 227543,
+			"friends_count": 7475,
+			"listed_count": 318,
+			"created_at": "Thu Jan 26 01:48:30 +0000 2012",
+			"favourites_count": 30236,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 37611,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1781632157/Titusoreily_portrait_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1781632157/Titusoreily_portrait_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/474493404/1540181205",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 329,
+		"favorite_count": 3116,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Jaidyn Stephenson has been handed a 22-match ban (12 matches suspended) and fined $20,000. I'm disappointed as I'd backed him to get seven weeks through the AFL's official wagering partner."
+	},
+	{
 		"created_at": "Wed Jun 19 12:55:25 +0000 2019",
 		"id": 1141328633564454900,
 		"id_str": "1141328633564454912",
