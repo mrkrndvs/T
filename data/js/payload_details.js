95fff0d8906f495d10a42df009586f85de44e8bb
Aaron Davis <mrkrndvs@gmail.com>
2019-08-05 20:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31501,
-	"created_at": "Mon Aug 05 08:14:58 +0000 2019",
+	"tweets": 31502,
+	"created_at": "Mon Aug 05 10:14:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Mon Aug 05 09:56:49 +0000 2019",
+		"id": 1158016266021605400,
+		"id_str": "1158315915840126976",
+		"full_text": "If anyone would like to add a story (words/pics) to celebrate @daibarnes life - please DM me,  and I'll share a location to capture the lovely anecdotes - so that we can pass them on to his family so that they can see how positive his life was. #RIPDai Pls RT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RIPDai",
+					"indices": [
+						245,
+						252
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "daibarnes",
+					"name": "Dai Barnes 👣☀️🌧️",
+					"id": 14143403,
+					"id_str": "14143403",
+					"indices": [
+						62,
+						72
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16361307,
+			"id_str": "16361307",
+			"name": "Eylan Ezekiel FCCT ✊🏽🌳🖖🏽",
+			"screen_name": "eylanezekiel",
+			"location": "Oxford (most of the time)",
+			"description": "Edtech for @OxOutreach. Fellow of @CharteredColl ,  Trustee @a_new_direction. Positive Deviant. Ultimate Frisbee. RTs / Likes are not always an endorsement.",
+			"url": "https://t.co/gh9CA7fiI4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gh9CA7fiI4",
+							"expanded_url": "http://www.ezekiels.co.uk/eylan/",
+							"display_url": "ezekiels.co.uk/eylan/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3852,
+			"friends_count": 2732,
+			"listed_count": 229,
+			"created_at": "Fri Sep 19 09:17:57 +0000 2008",
+			"favourites_count": 7929,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 27274,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1157789854756982784/YfUfM6PT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1157789854756982784/YfUfM6PT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16361307/1533732118",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "If anyone would like to add a story (words/pics) to celebrate @daibarnes life - please DM me,  and I'll share a location to capture the lovely anecdotes - so that we can pass them on to his family so that they can see how positive his life was. #RIPDai Pls RT"
+	},
+	{
 		"created_at": "Mon Aug 05 08:01:32 +0000 2019",
 		"id": 1158286904992895000,
 		"id_str": "1158286904992894976",
