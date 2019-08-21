2c006c92eaa70c7193d70c6197f68f60052af733
Aaron Davis <mrkrndvs@gmail.com>
2019-08-21 23:15:00 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31520,
-	"created_at": "Wed Aug 21 11:14:59 +0000 2019",
+	"tweets": 31521,
+	"created_at": "Wed Aug 21 13:14:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,10 +1,136 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 21 12:20:50 +0000 2019",
+		"id": 1164150364179796000,
+		"id_str": "1164150364179795969",
+		"full_text": "@kottke That is a lot of Lego rail CC @justlego101",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kottke",
+					"name": "kottke.org",
+					"id": 14120215,
+					"id_str": "14120215",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "justlego101",
+					"name": "justlego101",
+					"id": 3122282574,
+					"id_str": "3122282574",
+					"indices": [
+						38,
+						50
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1163905044066046000,
+		"in_reply_to_status_id_str": "1163905044066045953",
+		"in_reply_to_user_id": 14120215,
+		"in_reply_to_user_id_str": "14120215",
+		"in_reply_to_screen_name": "kottke",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4267,
+			"friends_count": 989,
+			"listed_count": 378,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4878,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27847,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@kottke That is a lot of Lego rail CC @justlego101"
+	},
+	{
 		"created_at": "Wed Aug 21 10:39:53 +0000 2019",
 		"id": 1164124961151185000,
 		"id_str": "1164124961151184896",
-		"full_text": "@datruss Left some 'spam' (that is what Disqus deemed it) on your post. However, it can be found here too 🤷‍♂️ https://t.co/GFo14aGxHy",
+		"full_text": "@datruss Left some 'spam' (that is what Disqus deemed it) on your post. However, it can be found here too 🤷♂️ https://t.co/GFo14aGxHy",
 		"truncated": false,
 		"display_text_range": [
 			9,
@@ -125,7 +251,7 @@ Grailbird.data.tweets_2019_08 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "@datruss Left some 'spam' (that is what Disqus deemed it) on your post. However, it can be found here too 🤷‍♂️ https://t.co/GFo14aGxHy"
+		"text": "@datruss Left some 'spam' (that is what Disqus deemed it) on your post. However, it can be found here too 🤷♂️ https://t.co/GFo14aGxHy"
 	},
 	{
 		"created_at": "Sun Aug 18 10:28:04 +0000 2019",
