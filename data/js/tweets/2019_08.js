7db1648a74c9816d533caae4aa3c63dfd7b53158
Aaron Davis <mrkrndvs@gmail.com>
2019-08-31 18:15:00 +1000

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 31 07:30:59 +0000 2019",
+		"id": 1167255967228035000,
+		"id_str": "1167701300936466433",
+		"full_text": "Does anyone know of a school around Melbourne that could use 26 of these tables and/or chairs? Pick up would be from Monash Uni, Clayton and would have to be organised before next Friday. #VicPLN https://t.co/q9S7f6cpyY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "VicPLN",
+					"indices": [
+						188,
+						195
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1167255960366145500,
+					"id_str": "1167255960366145536",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EDLtHI-U4AAYuwC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDLtHI-U4AAYuwC.jpg",
+					"url": "https://t.co/q9S7f6cpyY",
+					"display_url": "pic.twitter.com/q9S7f6cpyY",
+					"expanded_url": "https://twitter.com/lucasjedu/status/1167255967228035072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1167255960366145500,
+					"id_str": "1167255960366145536",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EDLtHI-U4AAYuwC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDLtHI-U4AAYuwC.jpg",
+					"url": "https://t.co/q9S7f6cpyY",
+					"display_url": "pic.twitter.com/q9S7f6cpyY",
+					"expanded_url": "https://twitter.com/lucasjedu/status/1167255967228035072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2880054302,
+			"id_str": "2880054302",
+			"name": "Lucas Johnson",
+			"screen_name": "lucasjedu",
+			"location": "",
+			"description": "Research and Development in STEM Education. Dad, teacher, learner. All thoughts are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 518,
+			"friends_count": 329,
+			"listed_count": 8,
+			"created_at": "Mon Oct 27 22:25:01 +0000 2014",
+			"favourites_count": 755,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 374,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1118777863908413440/oG2ljXC4_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1118777863908413440/oG2ljXC4_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2880054302/1474511536",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Does anyone know of a school around Melbourne that could use 26 of these tables and/or chairs? Pick up would be from Monash Uni, Clayton and would have to be organised before next Friday. #VicPLN https://t.co/q9S7f6cpyY"
+	},
+	{
 		"created_at": "Fri Aug 30 02:10:00 +0000 2019",
 		"id": 1167258133753819100,
 		"id_str": "1167258133753819137",
