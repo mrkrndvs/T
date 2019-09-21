536c187fc5a6321572032beb74abdcb84447cbc6
Aaron Davis <mrkrndvs@gmail.com>
2019-09-22 09:15:00 +1000

    Added 1 tweets

--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 21 22:20:53 +0000 2019",
+		"id": 1175535394516955100,
+		"id_str": "1175535394516955136",
+		"full_text": "@ABCReligion Do we anchor on the idea of who ‘Sigmund Freud’ was or is? Is there a core truth to his work? I would argue that his thinking evolved overtime? To talk about Freud (or Marx or Derrida) is to make an assumption about who or what we are in fact talking about https://t.co/sb18rO7u5M",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			293
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ABCReligion",
+					"name": "ABC Religion&Ethics",
+					"id": 22092333,
+					"id_str": "22092333",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/sb18rO7u5M",
+					"expanded_url": "https://collect.readwriterespond.com/freud-thought-philosophers-were-deluded-but-was-he-one-himself-abc-news-australian-broadcasting-corporation/",
+					"display_url": "collect.readwriterespond.com/freud-thought-…",
+					"indices": [
+						270,
+						293
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1174066264165294000,
+		"in_reply_to_status_id_str": "1174066264165294081",
+		"in_reply_to_user_id": 22092333,
+		"in_reply_to_user_id_str": "22092333",
+		"in_reply_to_screen_name": "ABCReligion",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4269,
+			"friends_count": 989,
+			"listed_count": 376,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4889,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27894,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ABCReligion Do we anchor on the idea of who ‘Sigmund Freud’ was or is? Is there a core truth to his work? I would argue that his thinking evolved overtime? To talk about Freud (or Marx or Derrida) is to make an assumption about who or what we are in fact talking about https://t.co/sb18rO7u5M"
+	},
+	{
 		"created_at": "Sat Sep 21 12:56:21 +0000 2019",
 		"id": 1175393325026807800,
 		"id_str": "1175393325026807808",
