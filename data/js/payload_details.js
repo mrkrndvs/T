f6eb3b53ff16e3b9d4c805d998eb4b4e25e67c8e
Aaron Davis <mrkrndvs@gmail.com>
2019-09-27 16:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31568,
-	"created_at": "Sun Sep 22 22:14:59 +0000 2019",
+	"tweets": 31569,
+	"created_at": "Fri Sep 27 06:14:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 27 05:54:39 +0000 2019",
+		"id": 1177461528410198000,
+		"id_str": "1177461528410198018",
+		"full_text": "@doncandon I agree with your sentiments about local acts in regards to AFL/NRL, but I still think that @thekillers nailed it a few years ago: \n\n- A current single that worked\n- An edgy cover\n- An impromptu duet\n\nhttps://t.co/d5jMzmf1Ey",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			235
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "doncandon",
+					"name": "Dan Condon",
+					"id": 1413861,
+					"id_str": "1413861",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "thekillers",
+					"name": "The Killers",
+					"id": 19661294,
+					"id_str": "19661294",
+					"indices": [
+						103,
+						114
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/d5jMzmf1Ey",
+					"expanded_url": "https://collect.readwriterespond.com/i-love-rugby-league/",
+					"display_url": "collect.readwriterespond.com/i-love-rugby-l…",
+					"indices": [
+						212,
+						235
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 1413861,
+		"in_reply_to_user_id_str": "1413861",
+		"in_reply_to_screen_name": "doncandon",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4265,
+			"friends_count": 988,
+			"listed_count": 373,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4891,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27896,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@doncandon I agree with your sentiments about local acts in regards to AFL/NRL, but I still think that @thekillers nailed it a few years ago: \n\n- A current single that worked\n- An edgy cover\n- An impromptu duet\n\nhttps://t.co/d5jMzmf1Ey"
+	},
+	{
 		"created_at": "Sun Sep 22 22:01:42 +0000 2019",
 		"id": 1175892957716734000,
 		"id_str": "1175892957716733953",
