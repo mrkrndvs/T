90500ce0f15da1c57c5875f0fdd0fdfb98e3e4cb
Aaron Davis <mrkrndvs@gmail.com>
2019-10-06 12:14:59 +1100

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sun Oct 06 00:49:59 +0000 2019",
+		"id": 1180646350645383200,
+		"id_str": "1180646350645383169",
+		"full_text": "@davecormier Did someone say ... #Rhizo20\n\nIt makes me wonder what is pertinent in regards to rhizomatic learning in 2020? How has the context changed? Is 'rhizomatic learning' atemporal? https://t.co/4XGRBmXzA5",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			187
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Rhizo20",
+					"indices": [
+						33,
+						41
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "davecormier",
+					"name": "dave cormier",
+					"id": 789991,
+					"id_str": "789991",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1180646340000223200,
+					"id_str": "1180646340000223232",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EGJ_l0UUEAA7HQa.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EGJ_l0UUEAA7HQa.jpg",
+					"url": "https://t.co/4XGRBmXzA5",
+					"display_url": "pic.twitter.com/4XGRBmXzA5",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1180646350645383169/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 306,
+							"h": 284,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 306,
+							"h": 284,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 306,
+							"h": 284,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1180646340000223200,
+					"id_str": "1180646340000223232",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EGJ_l0UUEAA7HQa.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EGJ_l0UUEAA7HQa.jpg",
+					"url": "https://t.co/4XGRBmXzA5",
+					"display_url": "pic.twitter.com/4XGRBmXzA5",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1180646350645383169/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 306,
+							"h": 284,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 306,
+							"h": 284,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 306,
+							"h": 284,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							153,
+							142
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EGJ_l0UUEAA7HQa.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1177346367079026700,
+		"in_reply_to_status_id_str": "1177346367079026688",
+		"in_reply_to_user_id": 789991,
+		"in_reply_to_user_id_str": "789991",
+		"in_reply_to_screen_name": "davecormier",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4264,
+			"friends_count": 988,
+			"listed_count": 372,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4890,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27905,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@davecormier Did someone say ... #Rhizo20\n\nIt makes me wonder what is pertinent in regards to rhizomatic learning in 2020? How has the context changed? Is 'rhizomatic learning' atemporal? https://t.co/4XGRBmXzA5"
+	},
+	{
 		"created_at": "Sat Oct 05 23:23:43 +0000 2019",
 		"id": 1176155810944012300,
 		"id_str": "1180624638914617344",
