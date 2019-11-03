5daf3a8d3d686f1ca5ca0f5bd4fa016c45e92ecf
Aaron Davis <mrkrndvs@gmail.com>
2019-11-03 20:14:59 +1100

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sun Nov 03 08:54:00 +0000 2019",
+		"id": 1190915015257116700,
+		"id_str": "1190915015257116672",
+		"full_text": "@debsnet @JanOwenAM @EdRev @BruceWellman @ScienceChiefAu @audreywatters @brookssensei @flipthesystemoz As one of those ‘little things’ coming into schools it can be easy to bring in a script. Problem is that each school is made up of many other ‘little things’. I have therefore found it more useful to gauge as much about the schools context and start there https://t.co/HsBm2czg1b",
+		"truncated": false,
+		"display_text_range": [
+			103,
+			382
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "debsnet",
+					"name": "𝗗𝗿 𝗗𝗲𝗯𝗼𝗿𝗮𝗵 𝗡𝗲𝘁𝗼𝗹𝗶𝗰𝗸𝘆",
+					"id": 88375038,
+					"id_str": "88375038",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "JanOwenAM",
+					"name": "Jan Owen",
+					"id": 295508085,
+					"id_str": "295508085",
+					"indices": [
+						9,
+						19
+					]
+				},
+				{
+					"screen_name": "EdRev",
+					"name": "Peter Hutton",
+					"id": 1637363858,
+					"id_str": "1637363858",
+					"indices": [
+						20,
+						26
+					]
+				},
+				{
+					"screen_name": "BruceWellman",
+					"name": "Bruce Wellman",
+					"id": 88578195,
+					"id_str": "88578195",
+					"indices": [
+						27,
+						40
+					]
+				},
+				{
+					"screen_name": "ScienceChiefAu",
+					"name": "Chief Scientist",
+					"id": 3158689771,
+					"id_str": "3158689771",
+					"indices": [
+						41,
+						56
+					]
+				},
+				{
+					"screen_name": "audreywatters",
+					"name": "Audrey Watters",
+					"id": 25388528,
+					"id_str": "25388528",
+					"indices": [
+						57,
+						71
+					]
+				},
+				{
+					"screen_name": "brookssensei",
+					"name": "Adam Brooks",
+					"id": 1591118918,
+					"id_str": "1591118918",
+					"indices": [
+						72,
+						85
+					]
+				},
+				{
+					"screen_name": "flipthesystemoz",
+					"name": "Flip the System Oz",
+					"id": 1000175125608583200,
+					"id_str": "1000175125608583168",
+					"indices": [
+						86,
+						102
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HsBm2czg1b",
+					"expanded_url": "https://collect.readwriterespond.com/innovation-in-schools/",
+					"display_url": "collect.readwriterespond.com/innovation-in-…",
+					"indices": [
+						359,
+						382
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1186282540253888500,
+		"in_reply_to_status_id_str": "1186282540253888512",
+		"in_reply_to_user_id": 88375038,
+		"in_reply_to_user_id_str": "88375038",
+		"in_reply_to_screen_name": "debsnet",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4257,
+			"friends_count": 990,
+			"listed_count": 373,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4895,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27930,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@debsnet @JanOwenAM @EdRev @BruceWellman @ScienceChiefAu @audreywatters @brookssensei @flipthesystemoz As one of those ‘little things’ coming into schools it can be easy to bring in a script. Problem is that each school is made up of many other ‘little things’. I have therefore found it more useful to gauge as much about the schools context and start there https://t.co/HsBm2czg1b"
+	},
+	{
 		"created_at": "Sat Nov 02 09:43:20 +0000 2019",
 		"id": 1190565043051495400,
 		"id_str": "1190565043051495424",
