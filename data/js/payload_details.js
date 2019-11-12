2deb5ecf118fb452bd1f0c8b7ccb535b46c907c2
Aaron Davis <mrkrndvs@gmail.com>
2019-11-12 23:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31612,
-	"created_at": "Tue Nov 12 11:14:58 +0000 2019",
+	"tweets": 31613,
+	"created_at": "Tue Nov 12 12:14:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,155 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Tue Nov 12 11:18:44 +0000 2019",
+		"id": 1194212928473325600,
+		"id_str": "1194212928473325569",
+		"full_text": "@drlindagraham @oliviatomess Linda, your point about rigor reminds me of the recent piece by @LydiaDenworth on the need for more nuance and research https://t.co/R6iMwVINAI",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			172
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "drlindagraham",
+					"name": "Linda J. Graham",
+					"id": 613376460,
+					"id_str": "613376460",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "oliviatomess",
+					"name": "Olivia",
+					"id": 918293518648467500,
+					"id_str": "918293518648467456",
+					"indices": [
+						15,
+						28
+					]
+				},
+				{
+					"screen_name": "LydiaDenworth",
+					"name": "Lydia Denworth",
+					"id": 1914330152,
+					"id_str": "1914330152",
+					"indices": [
+						93,
+						107
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/R6iMwVINAI",
+					"expanded_url": "https://collect.readwriterespond.com/social-media-has-not-destroyed-a-generation/",
+					"display_url": "collect.readwriterespond.com/social-media-h…",
+					"indices": [
+						149,
+						172
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1193849412499280000,
+		"in_reply_to_status_id_str": "1193849412499279873",
+		"in_reply_to_user_id": 613376460,
+		"in_reply_to_user_id_str": "613376460",
+		"in_reply_to_screen_name": "drlindagraham",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4256,
+			"friends_count": 990,
+			"listed_count": 373,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4900,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27942,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@drlindagraham @oliviatomess Linda, your point about rigor reminds me of the recent piece by @LydiaDenworth on the need for more nuance and research https://t.co/R6iMwVINAI"
+	},
+	{
 		"created_at": "Tue Nov 12 11:10:23 +0000 2019",
 		"id": 1194210828209283000,
 		"id_str": "1194210828209283072",
