0320799bd59d9a8d68aad1544aeddd3fd38e685e
Aaron Davis <mrkrndvs@gmail.com>
2019-11-22 15:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31621,
-	"created_at": "Thu Nov 21 23:14:58 +0000 2019",
+	"tweets": 31622,
+	"created_at": "Fri Nov 22 04:14:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 22 03:36:24 +0000 2019",
+		"id": 1197720458906370000,
+		"id_str": "1197720458906370049",
+		"full_text": "@betchaboy Well that answers that I guess, even if it does seem that I am logged in. \n\nSo as the files are stored in Google Drive, is Google Earth simply a means of viewing/presenting the data? https://t.co/027yIHBjbo",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			193
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "betchaboy",
+					"name": "Chris Betcher",
+					"id": 779452,
+					"id_str": "779452",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1197720454737223700,
+					"id_str": "1197720454737223680",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ8oaGTU4AAFdiW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJ8oaGTU4AAFdiW.jpg",
+					"url": "https://t.co/027yIHBjbo",
+					"display_url": "pic.twitter.com/027yIHBjbo",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1197720458906370049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 318,
+							"h": 513,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 318,
+							"h": 513,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 318,
+							"h": 513,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1197720454737223700,
+					"id_str": "1197720454737223680",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ8oaGTU4AAFdiW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJ8oaGTU4AAFdiW.jpg",
+					"url": "https://t.co/027yIHBjbo",
+					"display_url": "pic.twitter.com/027yIHBjbo",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1197720458906370049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 318,
+							"h": 513,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 318,
+							"h": 513,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 318,
+							"h": 513,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1197662784017719300,
+		"in_reply_to_status_id_str": "1197662784017719297",
+		"in_reply_to_user_id": 779452,
+		"in_reply_to_user_id_str": "779452",
+		"in_reply_to_screen_name": "betchaboy",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4255,
+			"friends_count": 990,
+			"listed_count": 372,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4901,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27953,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@betchaboy Well that answers that I guess, even if it does seem that I am logged in. \n\nSo as the files are stored in Google Drive, is Google Earth simply a means of viewing/presenting the data? https://t.co/027yIHBjbo"
+	},
+	{
 		"created_at": "Thu Nov 21 22:21:34 +0000 2019",
 		"id": 1197641226452791300,
 		"id_str": "1197641226452791296",
