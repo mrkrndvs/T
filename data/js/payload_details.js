f648eee95a194b5527ece2ad3ebab841d49dc74d
Aaron Davis <mrkrndvs@gmail.com>
2020-01-12 08:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31647,
-	"created_at": "Wed Jan 08 23:14:58 +0000 2020",
+	"tweets": 31648,
+	"created_at": "Sat Jan 11 21:14:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Sat Jan 11 20:56:59 +0000 2020",
+		"id": 1216101723472711700,
+		"id_str": "1216101723472711680",
+		"full_text": "@readtedium Does software fit into this as well? It is interesting to think of this alongside @doctorow 's discussion of adversarial interoperability. I wonder if there is a model that actually works for businesses or if it is something that happens bit by bit by chance?",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "readtedium",
+					"name": "Tedium",
+					"id": 2903269822,
+					"id_str": "2903269822",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "doctorow",
+					"name": "Bernie Beats Trump",
+					"id": 2729061,
+					"id_str": "2729061",
+					"indices": [
+						94,
+						103
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1214894865219248000,
+		"in_reply_to_status_id_str": "1214894865219248128",
+		"in_reply_to_user_id": 2903269822,
+		"in_reply_to_user_id_str": "2903269822",
+		"in_reply_to_screen_name": "readtedium",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4249,
+			"friends_count": 991,
+			"listed_count": 370,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4899,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27974,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@readtedium Does software fit into this as well? It is interesting to think of this alongside @doctorow 's discussion of adversarial interoperability. I wonder if there is a model that actually works for businesses or if it is something that happens bit by bit by chance?"
+	},
+	{
 		"created_at": "Wed Jan 08 22:30:19 +0000 2020",
 		"id": 1215038047529472000,
 		"id_str": "1215038047529472000",
@@ -132,7 +260,7 @@ Grailbird.data.tweets_2020_01 =
 			"created_at": "Wed Jan 08 14:45:01 +0000 2020",
 			"id": 1214920950510182400,
 			"id_str": "1214920950510182400",
-			"full_text": "Announcing my new project for 2020: https://t.co/nVsWXGyXyJ\n\nResources and workshops to help companies migrate from Excel to #GoogleSheets\n\n📙 Read the free 8,000 word guide\n\n👨‍💻 Book a workshop \n\nInfo here 👉 https://t.co/42Px3XuWNH",
+			"full_text": "Announcing my new project for 2020: https://t.co/nVsWXGyXyJ\n\nResources and workshops to help companies migrate from Excel to #GoogleSheets\n\n📙 Read the free 8,000 word guide\n\n👨💻 Book a workshop \n\nInfo here 👉 https://t.co/42Px3XuWNH",
 			"truncated": false,
 			"display_text_range": [
 				0,
