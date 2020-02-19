bb24359492445dea05635ae8e9e2a50eb45364b4
Aaron Davis <mrkrndvs@gmail.com>
2020-02-20 08:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31675,
-	"created_at": "Wed Feb 19 12:14:59 +0000 2020",
+	"tweets": 31676,
+	"created_at": "Wed Feb 19 21:14:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 19 20:25:09 +0000 2020",
+		"id": 1230226840582705200,
+		"id_str": "1230226840582705153",
+		"full_text": "@SwitchedOnPop @halsey @5SOS @sushitrash It would be interesting to see how many songs have their first beginnings on guitar, but it then gets stripped out in production. For example, consider Taylor Swift's cut back version of The Man. This is not a 'guitar' song, but could quite easily could have been.",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			305
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SwitchedOnPop",
+					"name": "Switched on Pop 🎶🎧💥",
+					"id": 2881229840,
+					"id_str": "2881229840",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "halsey",
+					"name": "h",
+					"id": 45709328,
+					"id_str": "45709328",
+					"indices": [
+						15,
+						22
+					]
+				},
+				{
+					"screen_name": "5SOS",
+					"name": "5 Seconds of Summer",
+					"id": 264107729,
+					"id_str": "264107729",
+					"indices": [
+						23,
+						28
+					]
+				},
+				{
+					"screen_name": "sushitrash",
+					"name": "JOJI",
+					"id": 2579706949,
+					"id_str": "2579706949",
+					"indices": [
+						29,
+						40
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1229841215312535600,
+		"in_reply_to_status_id_str": "1229841215312535555",
+		"in_reply_to_user_id": 2881229840,
+		"in_reply_to_user_id_str": "2881229840",
+		"in_reply_to_screen_name": "SwitchedOnPop",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4249,
+			"friends_count": 991,
+			"listed_count": 367,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4903,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28000,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SwitchedOnPop @halsey @5SOS @sushitrash It would be interesting to see how many songs have their first beginnings on guitar, but it then gets stripped out in production. For example, consider Taylor Swift's cut back version of The Man. This is not a 'guitar' song, but could quite easily could have been."
+	},
+	{
 		"created_at": "Wed Feb 19 11:25:40 +0000 2020",
 		"id": 1230091072384028700,
 		"id_str": "1230091072384028677",
