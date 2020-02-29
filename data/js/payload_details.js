2d9001b5565e02e48e5b8f059f6be16c57cafb68
Aaron Davis <mrkrndvs@gmail.com>
2020-02-29 22:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31683,
-	"created_at": "Wed Feb 26 11:14:59 +0000 2020",
+	"tweets": 31684,
+	"created_at": "Sat Feb 29 11:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sat Feb 29 10:55:05 +0000 2020",
+		"id": 1233707256950083600,
+		"id_str": "1233707256950083585",
+		"full_text": "@rakt Fantastic to have you back on the airwaves @rakt and @AdamLavars. Honest and enthralling as always. I left a comment on the blog, also syndicated here https://t.co/7xhDO9CoEq Great to have the specialist perspective included 👍",
+		"truncated": false,
+		"display_text_range": [
+			6,
+			232
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rakt",
+					"name": "Rick Kayler-Thomson",
+					"id": 21286176,
+					"id_str": "21286176",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "rakt",
+					"name": "Rick Kayler-Thomson",
+					"id": 21286176,
+					"id_str": "21286176",
+					"indices": [
+						49,
+						54
+					]
+				},
+				{
+					"screen_name": "AdamLavars",
+					"name": "Adam Lavars",
+					"id": 34422666,
+					"id_str": "34422666",
+					"indices": [
+						59,
+						70
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7xhDO9CoEq",
+					"expanded_url": "https://collect.readwriterespond.com/?p=14766",
+					"display_url": "collect.readwriterespond.com/?p=14766",
+					"indices": [
+						157,
+						180
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1230448633964683300,
+		"in_reply_to_status_id_str": "1230448633964683265",
+		"in_reply_to_user_id": 21286176,
+		"in_reply_to_user_id_str": "21286176",
+		"in_reply_to_screen_name": "rakt",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4249,
+			"friends_count": 991,
+			"listed_count": 366,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4904,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28010,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@rakt Fantastic to have you back on the airwaves @rakt and @AdamLavars. Honest and enthralling as always. I left a comment on the blog, also syndicated here https://t.co/7xhDO9CoEq Great to have the specialist perspective included 👍"
+	},
+	{
 		"created_at": "Wed Feb 26 10:30:22 +0000 2020",
 		"id": 1232613872709689300,
 		"id_str": "1232613872709689344",
