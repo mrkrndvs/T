055e10b11fd6106c066426c9b69527f992fe756c
Aaron Davis <mrkrndvs@gmail.com>
2020-03-22 16:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31711,
-	"created_at": "Sun Mar 22 04:14:58 +0000 2020",
+	"tweets": 31712,
+	"created_at": "Sun Mar 22 05:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,295 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 22 04:24:10 +0000 2020",
+		"id": 1241581412316135400,
+		"id_str": "1241581412316135424",
+		"full_text": "I remember @jackantonoff talking about writing 'Let's Get Married' in response to Trump being elected https://t.co/PnNkN4Uieo \nThe current crisis is a diff event to make sense of. In this week's new music podcast, @nprmusic talks about finding world's to escape into as response https://t.co/yJN3KNb2kf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jackantonoff",
+					"name": "jackantonoff",
+					"id": 414569910,
+					"id_str": "414569910",
+					"indices": [
+						11,
+						24
+					]
+				},
+				{
+					"screen_name": "nprmusic",
+					"name": "NPR Music",
+					"id": 13784592,
+					"id_str": "13784592",
+					"indices": [
+						214,
+						223
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/PnNkN4Uieo",
+					"expanded_url": "https://youtu.be/xHL2Cc5s83k",
+					"display_url": "youtu.be/xHL2Cc5s83k",
+					"indices": [
+						102,
+						125
+					]
+				},
+				{
+					"url": "https://t.co/yJN3KNb2kf",
+					"expanded_url": "https://twitter.com/nprmusic/status/1241017597472243712",
+					"display_url": "twitter.com/nprmusic/statu…",
+					"indices": [
+						279,
+						302
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4246,
+			"friends_count": 989,
+			"listed_count": 366,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4907,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28044,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1241017597472243700,
+		"quoted_status_id_str": "1241017597472243712",
+		"quoted_status_permalink": {
+			"url": "https://t.co/yJN3KNb2kf",
+			"expanded": "https://twitter.com/nprmusic/status/1241017597472243712",
+			"display": "twitter.com/nprmusic/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Mar 20 15:03:46 +0000 2020",
+			"id": 1241017597472243700,
+			"id_str": "1241017597472243712",
+			"full_text": "We know it's been an unfathomably long week, but it's Friday, which means @allsongs is here with a bunch of new music, including new albums from @JBALVIN and @theweeknd. \n\nWhat are you listening to today? https://t.co/m9EDKHOybE",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				228
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "allsongs",
+						"name": "All Songs Considered",
+						"id": 12844642,
+						"id_str": "12844642",
+						"indices": [
+							74,
+							83
+						]
+					},
+					{
+						"screen_name": "JBALVIN",
+						"name": "J BALVIN",
+						"id": 44670915,
+						"id_str": "44670915",
+						"indices": [
+							145,
+							153
+						]
+					},
+					{
+						"screen_name": "theweeknd",
+						"name": "The Weeknd",
+						"id": 255388236,
+						"id_str": "255388236",
+						"indices": [
+							158,
+							168
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/m9EDKHOybE",
+						"expanded_url": "https://trib.al/XcGlc23",
+						"display_url": "trib.al/XcGlc23",
+						"indices": [
+							205,
+							228
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 13784592,
+				"id_str": "13784592",
+				"name": "NPR Music",
+				"screen_name": "nprmusic",
+				"location": "NYC & Washington, DC",
+				"description": "For the love of music.",
+				"url": "https://t.co/WrHlSGCWfC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WrHlSGCWfC",
+								"expanded_url": "http://www.npr.org/music",
+								"display_url": "npr.org/music",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 691777,
+				"friends_count": 980,
+				"listed_count": 10629,
+				"created_at": "Thu Feb 21 19:33:37 +0000 2008",
+				"favourites_count": 2098,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 46359,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/768169011859324928/-2frWHaT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/768169011859324928/-2frWHaT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/13784592/1580828093",
+				"profile_link_color": "88A2E3",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 30,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I remember @jackantonoff talking about writing 'Let's Get Married' in response to Trump being elected https://t.co/PnNkN4Uieo \nThe current crisis is a diff event to make sense of. In this week's new music podcast, @nprmusic talks about finding world's to escape into as response https://t.co/yJN3KNb2kf"
+	},
+	{
 		"created_at": "Sun Mar 22 03:23:36 +0000 2020",
 		"id": 1241566169577873400,
 		"id_str": "1241566169577873410",
