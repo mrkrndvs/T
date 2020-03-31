100131e4274175ac0a648c6a4f36a4ddba66f6a3
Aaron Davis <mrkrndvs@gmail.com>
2020-04-01 08:15:01 +1100

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Tue Mar 31 21:12:04 +0000 2020",
+		"id": 1245096546405761000,
+		"id_str": "1245096546405761026",
+		"full_text": "@RachelWilson100 Rachel, is the fear that students expected to engage in social distancing at home because of school closures will not comply with this and instead socialise and potentially spread that way? This seems to be the message from Brendan Murphy on @4corners https://t.co/uA1Iru7EqU",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			292
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RachelWilson100",
+					"name": "Rachel Wilson",
+					"id": 333873136,
+					"id_str": "333873136",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "4corners",
+					"name": "4corners",
+					"id": 16212771,
+					"id_str": "16212771",
+					"indices": [
+						259,
+						268
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uA1Iru7EqU",
+					"expanded_url": "https://www.abc.net.au/4corners/4cs_murphyinterview_3003/12102638",
+					"display_url": "abc.net.au/4corners/4cs_m…",
+					"indices": [
+						269,
+						292
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1243410193611800600,
+		"in_reply_to_status_id_str": "1243410193611800577",
+		"in_reply_to_user_id": 333873136,
+		"in_reply_to_user_id_str": "333873136",
+		"in_reply_to_screen_name": "RachelWilson100",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4243,
+			"friends_count": 989,
+			"listed_count": 366,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4906,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28072,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@RachelWilson100 Rachel, is the fear that students expected to engage in social distancing at home because of school closures will not comply with this and instead socialise and potentially spread that way? This seems to be the message from Brendan Murphy on @4corners https://t.co/uA1Iru7EqU"
+	},
+	{
 		"created_at": "Tue Mar 31 11:24:02 +0000 2020",
 		"id": 1244948563307585500,
 		"id_str": "1244948563307585536",
