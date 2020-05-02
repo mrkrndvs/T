224f0a50cf62b61ee5ec9a879a4dc54c9a6ea839
Aaron Davis <mrkrndvs@gmail.com>
2020-05-02 15:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31784,
-	"created_at": "Sat May 02 03:14:58 +0000 2020",
+	"tweets": 31785,
+	"created_at": "Sat May 02 05:14:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,10 +1,126 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 02 04:28:10 +0000 2020",
+		"id": 1256440320650174500,
+		"id_str": "1256440320650174465",
+		"full_text": "Feel Ms9 was left inspired after reading @fionathehardy great novel How to Make a Movie in 12 Days about what is involved. In a strange piece of coincidence, the initial footage mysteriously disappeared from the device ...",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "fionathehardy",
+					"name": "Fiona Hardy",
+					"id": 208438518,
+					"id_str": "208438518",
+					"indices": [
+						41,
+						55
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1256417981266976800,
+		"in_reply_to_status_id_str": "1256417981266976768",
+		"in_reply_to_user_id": 372768752,
+		"in_reply_to_user_id_str": "372768752",
+		"in_reply_to_screen_name": "mrkrndvs",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4237,
+			"friends_count": 989,
+			"listed_count": 364,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4909,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28124,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Feel Ms9 was left inspired after reading @fionathehardy great novel How to Make a Movie in 12 Days about what is involved. In a strange piece of coincidence, the initial footage mysteriously disappeared from the device ..."
+	},
+	{
 		"created_at": "Sat May 02 02:59:24 +0000 2020",
 		"id": 1256417981266976800,
 		"id_str": "1256417981266976768",
-		"full_text": "Have spent the last month throwing ideas of projects for the girls to dive into. Finally something clicked yesterday w/ Ms4 announcing she wanted to make a movie. Why not🤷‍♂️ Since then things have just gotten serious. Green screen out, @TouchCastEdu going &amp; props strewn everywhere https://t.co/eZwLqScdTg",
+		"full_text": "Have spent the last month throwing ideas of projects for the girls to dive into. Finally something clicked yesterday w/ Ms4 announcing she wanted to make a movie. Why not🤷♂️ Since then things have just gotten serious. Green screen out, @TouchCastEdu going &amp; props strewn everywhere https://t.co/eZwLqScdTg",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -193,6 +309,6 @@ Grailbird.data.tweets_2020_05 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Have spent the last month throwing ideas of projects for the girls to dive into. Finally something clicked yesterday w/ Ms4 announcing she wanted to make a movie. Why not🤷‍♂️ Since then things have just gotten serious. Green screen out, @TouchCastEdu going &amp; props strewn everywhere https://t.co/eZwLqScdTg"
+		"text": "Have spent the last month throwing ideas of projects for the girls to dive into. Finally something clicked yesterday w/ Ms4 announcing she wanted to make a movie. Why not🤷♂️ Since then things have just gotten serious. Green screen out, @TouchCastEdu going &amp; props strewn everywhere https://t.co/eZwLqScdTg"
 	}
 ]
