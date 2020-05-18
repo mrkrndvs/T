45a48aebfcf9f1c7e9dafede72c364c4a66614a5
Aaron Davis <mrkrndvs@gmail.com>
2020-05-19 09:15:00 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31798,
-	"created_at": "Mon May 18 13:14:59 +0000 2020",
+	"tweets": 31799,
+	"created_at": "Mon May 18 23:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Mon May 18 22:29:02 +0000 2020",
+		"id": 1262510536748486700,
+		"id_str": "1262510536748486657",
+		"full_text": "@ErinBromage @datruss Thank you Erin for the clarification. Really appreciated your references in your post on risks https://t.co/H1UOw0Iid5 and your work in general\n\nWith current conjecture in Aus about schools https://t.co/jYv8BM1SJD it has me think that not all spaces are equal &amp; all risks the same",
+		"truncated": false,
+		"display_text_range": [
+			22,
+			306
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ErinBromage",
+					"name": "Erin Bromage",
+					"id": 759895155654684700,
+					"id_str": "759895155654684672",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "datruss",
+					"name": "David Truss",
+					"id": 10298552,
+					"id_str": "10298552",
+					"indices": [
+						13,
+						21
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/H1UOw0Iid5",
+					"expanded_url": "https://www.erinbromage.com/post/the-risks-know-them-avoid-them",
+					"display_url": "erinbromage.com/post/the-risks…",
+					"indices": [
+						117,
+						140
+					]
+				},
+				{
+					"url": "https://t.co/jYv8BM1SJD",
+					"expanded_url": "https://collect.readwriterespond.com/education-coronavirus-and-the-australian-government/#Paul+Kelly",
+					"display_url": "collect.readwriterespond.com/education-coro…",
+					"indices": [
+						212,
+						235
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1262371783233147000,
+		"in_reply_to_status_id_str": "1262371783233146882",
+		"in_reply_to_user_id": 759895155654684700,
+		"in_reply_to_user_id_str": "759895155654684672",
+		"in_reply_to_screen_name": "ErinBromage",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4235,
+			"friends_count": 990,
+			"listed_count": 358,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4903,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28137,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ErinBromage @datruss Thank you Erin for the clarification. Really appreciated your references in your post on risks https://t.co/H1UOw0Iid5 and your work in general\n\nWith current conjecture in Aus about schools https://t.co/jYv8BM1SJD it has me think that not all spaces are equal &amp; all risks the same"
+	},
+	{
 		"created_at": "Mon May 18 13:10:18 +0000 2020",
 		"id": 1262369926729236500,
 		"id_str": "1262369926729236481",
