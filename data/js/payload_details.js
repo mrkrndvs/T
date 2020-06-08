81f3352f87a5177e2eec59a8d800c99cb8928ae3
Aaron Davis <mrkrndvs@gmail.com>
2020-06-08 22:15:00 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31832,
-	"created_at": "Mon Jun 08 08:14:58 +0000 2020",
+	"tweets": 31833,
+	"created_at": "Mon Jun 08 12:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 08 11:24:55 +0000 2020",
+		"id": 1269953548420132900,
+		"id_str": "1269953548420132864",
+		"full_text": "@benthompson Ben, this call for everyone to have their own blog reminds me of @CalNewport 's recent discussion of the limits of expert Twitter https://t.co/3K7aerwTz2. I am also interested in @threadreaderapp recent support for of Micropub to aide blogging / archiving https://t.co/Kh6wCI6Ilb",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			292
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "benthompson",
+					"name": "Ben Thompson",
+					"id": 40273,
+					"id_str": "40273",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "Calnewport",
+					"name": "calnewport1",
+					"id": 1101512914190716900,
+					"id_str": "1101512914190716928",
+					"indices": [
+						78,
+						89
+					]
+				},
+				{
+					"screen_name": "threadreaderapp",
+					"name": "Thread Reader App",
+					"id": 895814938995957800,
+					"id_str": "895814938995957760",
+					"indices": [
+						192,
+						208
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/3K7aerwTz2",
+					"expanded_url": "https://www.wired.com/story/opinion-expert-twitter-only-goes-so-far-bring-back-blogs/",
+					"display_url": "wired.com/story/opinion-…",
+					"indices": [
+						143,
+						166
+					]
+				},
+				{
+					"url": "https://t.co/Kh6wCI6Ilb",
+					"expanded_url": "https://threadreaderapp.com/account/micropub",
+					"display_url": "threadreaderapp.com/account/microp…",
+					"indices": [
+						269,
+						292
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1269631831617319000,
+		"in_reply_to_status_id_str": "1269631831617318913",
+		"in_reply_to_user_id": 40273,
+		"in_reply_to_user_id_str": "40273",
+		"in_reply_to_screen_name": "benthompson",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4224,
+			"friends_count": 989,
+			"listed_count": 357,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4902,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28173,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@benthompson Ben, this call for everyone to have their own blog reminds me of @CalNewport 's recent discussion of the limits of expert Twitter https://t.co/3K7aerwTz2. I am also interested in @threadreaderapp recent support for of Micropub to aide blogging / archiving https://t.co/Kh6wCI6Ilb"
+	},
+	{
 		"created_at": "Mon Jun 08 07:27:34 +0000 2020",
 		"id": 1269893818737811500,
 		"id_str": "1269893818737811456",
