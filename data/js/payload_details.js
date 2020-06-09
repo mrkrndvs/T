22667da4e0529ec716e766f6968478b500afaa97
Aaron Davis <mrkrndvs@gmail.com>
2020-06-09 22:15:00 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31833,
-	"created_at": "Mon Jun 08 12:14:59 +0000 2020",
+	"tweets": 31834,
+	"created_at": "Tue Jun 09 12:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 09 12:06:34 +0000 2020",
+		"id": 1270326420762321000,
+		"id_str": "1270326420762320896",
+		"full_text": "@Fordwritesmusic @KTNoonan @RadioNational This was a great episode Andrew. Have been left thinking about Katie's wondering why so much has been done to sustain sport, whereas culture has been left to dwindle. Her suggestion about the importance of gambling is intriguing. CC @MayneReport",
+		"truncated": false,
+		"display_text_range": [
+			42,
+			287
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Fordwritesmusic",
+					"name": "Andrew Ford",
+					"id": 2881306603,
+					"id_str": "2881306603",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "KTNoonan",
+					"name": "Katie Noonan",
+					"id": 69477882,
+					"id_str": "69477882",
+					"indices": [
+						17,
+						26
+					]
+				},
+				{
+					"screen_name": "RadioNational",
+					"name": "ABC Radio National",
+					"id": 19940915,
+					"id_str": "19940915",
+					"indices": [
+						27,
+						41
+					]
+				},
+				{
+					"screen_name": "MayneReport",
+					"name": "Stephen Mayne",
+					"id": 33811563,
+					"id_str": "33811563",
+					"indices": [
+						275,
+						287
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1269416598617550800,
+		"in_reply_to_status_id_str": "1269416598617550848",
+		"in_reply_to_user_id": 2881306603,
+		"in_reply_to_user_id_str": "2881306603",
+		"in_reply_to_screen_name": "Fordwritesmusic",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4222,
+			"friends_count": 989,
+			"listed_count": 357,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4902,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28174,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Fordwritesmusic @KTNoonan @RadioNational This was a great episode Andrew. Have been left thinking about Katie's wondering why so much has been done to sustain sport, whereas culture has been left to dwindle. Her suggestion about the importance of gambling is intriguing. CC @MayneReport"
+	},
+	{
 		"created_at": "Mon Jun 08 11:24:55 +0000 2020",
 		"id": 1269953548420132900,
 		"id_str": "1269953548420132864",
