181e866b3624f8a3dbf7a4ad113de80d743b2074
Aaron Davis <mrkrndvs@gmail.com>
2020-07-06 20:15:00 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31863,
-	"created_at": "Sun Jul 05 12:14:58 +0000 2020",
+	"tweets": 31864,
+	"created_at": "Mon Jul 06 10:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Mon Jul 06 10:05:55 +0000 2020",
+		"id": 1280080529115635700,
+		"id_str": "1280080529115635712",
+		"full_text": "@mrsfint Thank you for sharing Emily. Some resources that might interest are @AustinKleon thoughts &amp; strategies associated w/ keeping a notebook https://t.co/Dfcjm3Q2iv Also, @rissL recently shared some reflections from Claire Saxby on the topic too https://t.co/wmqmAf06QJ",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mrsfint",
+					"name": "Emily Fintelman",
+					"id": 2544875118,
+					"id_str": "2544875118",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "austinkleon",
+					"name": "Austin Kleon",
+					"id": 9698942,
+					"id_str": "9698942",
+					"indices": [
+						77,
+						89
+					]
+				},
+				{
+					"screen_name": "rissL",
+					"name": "Narissa Leung",
+					"id": 16400158,
+					"id_str": "16400158",
+					"indices": [
+						179,
+						185
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Dfcjm3Q2iv",
+					"expanded_url": "https://austinkleon.com/tag/notebooks/",
+					"display_url": "austinkleon.com/tag/notebooks/",
+					"indices": [
+						149,
+						172
+					]
+				},
+				{
+					"url": "https://t.co/wmqmAf06QJ",
+					"expanded_url": "https://www.ozlitteacher.com.au/2020/05/31/writers-and-their-notebooks-claire-saxby/",
+					"display_url": "ozlitteacher.com.au/2020/05/31/wri…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1278206557373599700,
+		"in_reply_to_status_id_str": "1278206557373599746",
+		"in_reply_to_user_id": 2544875118,
+		"in_reply_to_user_id_str": "2544875118",
+		"in_reply_to_screen_name": "mrsfint",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4205,
+			"friends_count": 988,
+			"listed_count": 357,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4903,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28189,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@mrsfint Thank you for sharing Emily. Some resources that might interest are @AustinKleon thoughts &amp; strategies associated w/ keeping a notebook https://t.co/Dfcjm3Q2iv Also, @rissL recently shared some reflections from Claire Saxby on the topic too https://t.co/wmqmAf06QJ"
+	},
+	{
 		"created_at": "Sun Jul 05 11:56:22 +0000 2020",
 		"id": 1279745934792945700,
 		"id_str": "1279745934792945664",
