75298d0fa2d6cc470a68e1d0f5d3753c183027c4
Aaron Davis <mrkrndvs@gmail.com>
2020-07-11 13:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31873,
-	"created_at": "Sat Jul 11 02:14:58 +0000 2020",
+	"tweets": 31874,
+	"created_at": "Sat Jul 11 03:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 11 02:28:23 +0000 2020",
+		"id": 1281777326020149200,
+		"id_str": "1281777326020149248",
+		"full_text": "@doublejmornings @rufuswainwright @DoubleJRadio Really enjoyed Rufus Wainwright's Take 5 where all along the music comes first. For me, this is epitomised by the story of spending a million dollars on his first album. Reminds me of the many tales where artists actually lose money in achieving some sort of greatness.",
+		"truncated": false,
+		"display_text_range": [
+			48,
+			317
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "doublejmornings",
+					"name": "Double J Mornings",
+					"id": 20006373,
+					"id_str": "20006373",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "rufuswainwright",
+					"name": "Rufus Wainwright",
+					"id": 15279958,
+					"id_str": "15279958",
+					"indices": [
+						17,
+						33
+					]
+				},
+				{
+					"screen_name": "DoubleJRadio",
+					"name": "Double J",
+					"id": 10403562,
+					"id_str": "10403562",
+					"indices": [
+						34,
+						47
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1281358276790120400,
+		"in_reply_to_status_id_str": "1281358276790120449",
+		"in_reply_to_user_id": 20006373,
+		"in_reply_to_user_id_str": "20006373",
+		"in_reply_to_screen_name": "doublejmornings",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4201,
+			"friends_count": 988,
+			"listed_count": 358,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4908,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28200,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@doublejmornings @rufuswainwright @DoubleJRadio Really enjoyed Rufus Wainwright's Take 5 where all along the music comes first. For me, this is epitomised by the story of spending a million dollars on his first album. Reminds me of the many tales where artists actually lose money in achieving some sort of greatness."
+	},
+	{
 		"created_at": "Sat Jul 11 01:15:25 +0000 2020",
 		"id": 1281758964808441900,
 		"id_str": "1281758964808441856",
