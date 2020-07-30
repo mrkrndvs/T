389e8786ef9b845e2f8a4604005655a0b02bac01
Aaron Davis <mrkrndvs@gmail.com>
2020-07-30 16:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31899,
-	"created_at": "Wed Jul 29 12:15:00 +0000 2020",
+	"tweets": 31900,
+	"created_at": "Thu Jul 30 06:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 30 06:05:00 +0000 2020",
+		"id": 1288717208563064800,
+		"id_str": "1288717208563064832",
+		"full_text": "@mrtonymartin is it just me or are both U2 and St. Vincent been listening to the A' Chaps lately with their cover of @DamianCowellDC's 'definitive' version of Stairway to Heaven? (https://t.co/Wc3lKZH72A) https://t.co/0KOvB4JvqQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			228
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mrtonymartin",
+					"name": "Tony Martin",
+					"id": 94004565,
+					"id_str": "94004565",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "DamianCowellDC",
+					"name": "Damian Cowell",
+					"id": 47548271,
+					"id_str": "47548271",
+					"indices": [
+						117,
+						132
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Wc3lKZH72A",
+					"expanded_url": "http://geekaloud.com/Damian%20Cowells%20Podcast%20Machine/Arseless%20Chaps%20Ep4.mp3#t=3000",
+					"display_url": "geekaloud.com/Damian%20Cowel…",
+					"indices": [
+						180,
+						203
+					]
+				},
+				{
+					"url": "https://t.co/0KOvB4JvqQ",
+					"expanded_url": "https://www.stereogum.com/2092838/u2-st-vincent-stairway-to-heaven-covers/music/",
+					"display_url": "stereogum.com/2092838/u2-st-…",
+					"indices": [
+						205,
+						228
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 94004565,
+		"in_reply_to_user_id_str": "94004565",
+		"in_reply_to_screen_name": "mrtonymartin",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4192,
+			"friends_count": 987,
+			"listed_count": 357,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4911,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28227,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@mrtonymartin is it just me or are both U2 and St. Vincent been listening to the A' Chaps lately with their cover of @DamianCowellDC's 'definitive' version of Stairway to Heaven? (https://t.co/Wc3lKZH72A) https://t.co/0KOvB4JvqQ"
+	},
+	{
 		"created_at": "Wed Jul 29 11:37:55 +0000 2020",
 		"id": 1288438603165495300,
 		"id_str": "1288438603165495297",
