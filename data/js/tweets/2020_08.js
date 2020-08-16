35b0999c0787bb22307772b8539a57ae1cce2d3f
Aaron Davis <mrkrndvs@gmail.com>
2020-08-16 13:15:00 +1000

    Added 1 tweets

--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,161 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sun Aug 16 03:01:20 +0000 2020",
+		"id": 1294831583053021200,
+		"id_str": "1294831583053021184",
+		"full_text": "@datruss @_valeriei @Steve_Collis Agree David. Definitely not questioning change &amp; positive opportunities offered by the current situation. Just trying to mindful of the overstretched rubber band I guess. Grt conversation nonetheless. Feels like it sits within the #flipthesystem movement https://t.co/S8psV5WE4s",
+		"truncated": false,
+		"display_text_range": [
+			34,
+			316
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "flipthesystem",
+					"indices": [
+						269,
+						283
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "datruss",
+					"name": "David Truss",
+					"id": 10298552,
+					"id_str": "10298552",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "_valeriei",
+					"name": "Dr. Valerie Irvine (she/her)",
+					"id": 40426722,
+					"id_str": "40426722",
+					"indices": [
+						9,
+						19
+					]
+				},
+				{
+					"screen_name": "Steve_Collis",
+					"name": "Steve Collis",
+					"id": 14842142,
+					"id_str": "14842142",
+					"indices": [
+						20,
+						33
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/S8psV5WE4s",
+					"expanded_url": "http://www.flip-the-system.org/",
+					"display_url": "flip-the-system.org",
+					"indices": [
+						293,
+						316
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1294827606718595000,
+		"in_reply_to_status_id_str": "1294827606718595072",
+		"in_reply_to_user_id": 10298552,
+		"in_reply_to_user_id_str": "10298552",
+		"in_reply_to_screen_name": "datruss",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4198,
+			"friends_count": 988,
+			"listed_count": 358,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4913,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28244,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@datruss @_valeriei @Steve_Collis Agree David. Definitely not questioning change &amp; positive opportunities offered by the current situation. Just trying to mindful of the overstretched rubber band I guess. Grt conversation nonetheless. Feels like it sits within the #flipthesystem movement https://t.co/S8psV5WE4s"
+	},
+	{
 		"created_at": "Sun Aug 16 01:27:06 +0000 2020",
 		"id": 1294807868168798200,
 		"id_str": "1294807868168798208",
