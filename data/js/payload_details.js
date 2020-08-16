2f8f0d97fd7fc5b5f2292c0b5ad37ccb9e27427a
Aaron Davis <mrkrndvs@gmail.com>
2020-08-16 18:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31916,
-	"created_at": "Sun Aug 16 05:14:58 +0000 2020",
+	"tweets": 31917,
+	"created_at": "Sun Aug 16 08:14:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sun Aug 16 07:23:34 +0000 2020",
+		"id": 1294897576454418400,
+		"id_str": "1294897576454418432",
+		"full_text": "@CmunroOz All well here. Grateful to be virus free, have a backyard for the kids to escape into and a job where I am able to work from home. \n\nHere is a picture of the ongoing project that is the outdoor tent :)\n\nHope all is well with you and the pivot to online learning. Stay safe https://t.co/wrHk0Q3Ym1",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			282
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CmunroOz",
+					"name": "Chris Munro",
+					"id": 476340029,
+					"id_str": "476340029",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1294897572302041000,
+					"id_str": "1294897572302041089",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/EfhmezmUMAERO-0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfhmezmUMAERO-0.jpg",
+					"url": "https://t.co/wrHk0Q3Ym1",
+					"display_url": "pic.twitter.com/wrHk0Q3Ym1",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1294897576454418432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1294897572302041000,
+					"id_str": "1294897572302041089",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/EfhmezmUMAERO-0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfhmezmUMAERO-0.jpg",
+					"url": "https://t.co/wrHk0Q3Ym1",
+					"display_url": "pic.twitter.com/wrHk0Q3Ym1",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1294897576454418432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1294874329205751800,
+		"in_reply_to_status_id_str": "1294874329205751809",
+		"in_reply_to_user_id": 476340029,
+		"in_reply_to_user_id_str": "476340029",
+		"in_reply_to_screen_name": "CmunroOz",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4198,
+			"friends_count": 988,
+			"listed_count": 358,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4913,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28246,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@CmunroOz All well here. Grateful to be virus free, have a backyard for the kids to escape into and a job where I am able to work from home. \n\nHere is a picture of the ongoing project that is the outdoor tent :)\n\nHope all is well with you and the pivot to online learning. Stay safe https://t.co/wrHk0Q3Ym1"
+	},
+	{
 		"created_at": "Sun Aug 16 05:06:11 +0000 2020",
 		"id": 1294863001401479200,
 		"id_str": "1294863001401479168",
