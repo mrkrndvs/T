c83a5b4e5af02b4233b64ff6065e5fb131b7e07c
Aaron Davis <mrkrndvs@gmail.com>
2020-09-26 01:15:02 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31941,
-	"created_at": "Sat Sep 19 01:14:59 +0000 2020",
+	"tweets": 31942,
+	"created_at": "Fri Sep 25 15:15:00 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Fri Sep 25 11:58:59 +0000 2020",
+		"id": 1309462402174394400,
+		"id_str": "1309462402174394370",
+		"full_text": "@austinkleon your post on Lynch and ideas https://t.co/4NaFm0BFu8 reminded me Rick Rubin's interview with Kevin Parker. Rubin talks about Neil Young's method of recording ideas and gives permission to Parker to capture his ideas when he has them https://t.co/My0tNeXMB1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "austinkleon",
+					"name": "Austin Kleon",
+					"id": 9698942,
+					"id_str": "9698942",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/4NaFm0BFu8",
+					"expanded_url": "https://austinkleon.com/2020/09/24/david-lynch-on-ideas/",
+					"display_url": "austinkleon.com/2020/09/24/dav…",
+					"indices": [
+						42,
+						65
+					]
+				},
+				{
+					"url": "https://t.co/My0tNeXMB1",
+					"expanded_url": "https://collect.readwriterespond.com/tame-impala-broken-record-podcast/",
+					"display_url": "collect.readwriterespond.com/tame-impala-br…",
+					"indices": [
+						246,
+						269
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 9698942,
+		"in_reply_to_user_id_str": "9698942",
+		"in_reply_to_screen_name": "austinkleon",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4205,
+			"friends_count": 990,
+			"listed_count": 361,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4916,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28273,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@austinkleon your post on Lynch and ideas https://t.co/4NaFm0BFu8 reminded me Rick Rubin's interview with Kevin Parker. Rubin talks about Neil Young's method of recording ideas and gives permission to Parker to capture his ideas when he has them https://t.co/My0tNeXMB1"
+	},
+	{
 		"created_at": "Sat Sep 19 01:12:45 +0000 2020",
 		"id": 1307125444609802200,
 		"id_str": "1307125444609802241",
