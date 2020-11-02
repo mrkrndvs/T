8719360f75890d03603427254137090f8278d4d0
Aaron Davis <mrkrndvs@gmail.com>
2020-11-03 09:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31972,
-	"created_at": "Mon Nov 02 11:14:59 +0000 2020",
+	"tweets": 31973,
+	"created_at": "Mon Nov 02 22:14:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 02 21:43:27 +0000 2020",
+		"id": 1323380224470339600,
+		"id_str": "1323380224470339584",
+		"full_text": "It is interesting to consider this rise in proctoring alongside other strategies, such as plagiarism detection software (https://t.co/gtN3zCAOFU) and cameras being used for attendance purposes (https://t.co/ScVbNtjJBR) https://t.co/xTx0vroEhI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			218
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gtN3zCAOFU",
+					"expanded_url": "https://collect.readwriterespond.com/automating-mistrust/",
+					"display_url": "collect.readwriterespond.com/automating-mis…",
+					"indices": [
+						121,
+						144
+					]
+				},
+				{
+					"url": "https://t.co/ScVbNtjJBR",
+					"expanded_url": "https://collect.readwriterespond.com/read-write-respond-021/#Using+Hitachi",
+					"display_url": "collect.readwriterespond.com/read-write-res…",
+					"indices": [
+						194,
+						217
+					]
+				},
+				{
+					"url": "https://t.co/xTx0vroEhI",
+					"expanded_url": "https://twitter.com/awrd/status/1322792657735745537",
+					"display_url": "twitter.com/awrd/status/13…",
+					"indices": [
+						219,
+						242
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4205,
+			"friends_count": 990,
+			"listed_count": 360,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28305,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1322792657735745500,
+		"quoted_status_id_str": "1322792657735745537",
+		"quoted_status_permalink": {
+			"url": "https://t.co/xTx0vroEhI",
+			"expanded": "https://twitter.com/awrd/status/1322792657735745537",
+			"display": "twitter.com/awrd/status/13…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Nov 01 06:48:40 +0000 2020",
+			"id": 1322792657735745500,
+			"id_str": "1322792657735745537",
+			"full_text": "\"We talk a lot about the rise of surveillance capitalism and ponder the grim future to which that Orwellian path leads. But for students? That future is now, as they try to act dutiful in front of their glowing webcams\". https://t.co/mjdHET1CeM",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				244
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mjdHET1CeM",
+						"expanded_url": "https://www.wired.com/story/ai-college-exam-proctors-surveillance/",
+						"display_url": "wired.com/story/ai-colle…",
+						"indices": [
+							221,
+							244
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14830831,
+				"id_str": "14830831",
+				"name": "Andrew Davies",
+				"screen_name": "awrd",
+				"location": "Brisbane, Australia",
+				"description": "Digital and Engagement Editor, ABC Audio Studios. Long-time audio producer and audiophile. All views mine not the ABC's.",
+				"url": "https://t.co/9DncuCorK1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/9DncuCorK1",
+								"expanded_url": "http://www.abc.net.au/podcasts/",
+								"display_url": "abc.net.au/podcasts/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3655,
+				"friends_count": 2006,
+				"listed_count": 248,
+				"created_at": "Mon May 19 10:19:10 +0000 2008",
+				"favourites_count": 1285,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 19351,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1122794088963592192/ZTdrRQ9p_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1122794088963592192/ZTdrRQ9p_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14830831/1543145384",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It is interesting to consider this rise in proctoring alongside other strategies, such as plagiarism detection software (https://t.co/gtN3zCAOFU) and cameras being used for attendance purposes (https://t.co/ScVbNtjJBR) https://t.co/xTx0vroEhI"
+	},
+	{
 		"created_at": "Mon Nov 02 10:29:20 +0000 2020",
 		"id": 1323210577314009000,
 		"id_str": "1323210577314009088",
