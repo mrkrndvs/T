ad7ea4760ffc309e325cc959f1f7a2117a025674
Aaron Davis <mrkrndvs@gmail.com>
2020-11-03 11:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31973,
-	"created_at": "Mon Nov 02 22:14:58 +0000 2020",
+	"tweets": 31974,
+	"created_at": "Tue Nov 03 00:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 02 23:27:34 +0000 2020",
+		"id": 1323406425880551400,
+		"id_str": "1323406425880551424",
+		"full_text": "This is a neat solution to managing virtual parent evenings. https://t.co/ysR8NjTBUm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ysR8NjTBUm",
+					"expanded_url": "https://twitter.com/Teacher_Luke_UK/status/1320618984639811586",
+					"display_url": "twitter.com/Teacher_Luke_U…",
+					"indices": [
+						61,
+						84
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4203,
+			"friends_count": 990,
+			"listed_count": 360,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28306,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1320618984639811600,
+		"quoted_status_id_str": "1320618984639811586",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ysR8NjTBUm",
+			"expanded": "https://twitter.com/Teacher_Luke_UK/status/1320618984639811586",
+			"display": "twitter.com/Teacher_Luke_U…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Oct 26 06:51:16 +0000 2020",
+			"id": 1320618984639811600,
+			"id_str": "1320618984639811586",
+			"full_text": "📢 To all Google schools considering a 'Virtual Parents' Evening', this #AppsScripts project can create Calendar events, Meet links and email them to parents. It saved us loads of time and reduced human error.\n\n📽 👉 https://t.co/16h5plyn0J\n\nLink 👉 https://t.co/zBKzyHuZCY https://t.co/RLOSeVGP5q",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				269
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "AppsScripts",
+						"indices": [
+							71,
+							83
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/16h5plyn0J",
+						"expanded_url": "https://youtu.be/cPbjikHwNlQ",
+						"display_url": "youtu.be/cPbjikHwNlQ",
+						"indices": [
+							214,
+							237
+						]
+					},
+					{
+						"url": "https://t.co/zBKzyHuZCY",
+						"expanded_url": "http://bit.ly/vpeassistant",
+						"display_url": "bit.ly/vpeassistant",
+						"indices": [
+							246,
+							269
+						]
+					},
+					{
+						"url": "https://t.co/RLOSeVGP5q",
+						"expanded_url": "https://twitter.com/Teacher_Luke_UK/status/1319323476801585152",
+						"display_url": "twitter.com/Teacher_Luke_U…",
+						"indices": [
+							270,
+							293
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1048815610778533900,
+				"id_str": "1048815610778533889",
+				"name": "Luke Craig",
+				"screen_name": "Teacher_Luke_UK",
+				"location": "London, England",
+				"description": "Y5 Teacher | Computing Lead Practitioner | Google Certified Trainer & 💡 #LON19 | Apple Teacher | MIE | M.Ed | #Edtech50 | Often tired, never bored",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3296,
+				"friends_count": 3372,
+				"listed_count": 16,
+				"created_at": "Sun Oct 07 06:01:42 +0000 2018",
+				"favourites_count": 2264,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 893,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1219360712142868488/5ldrsx1P_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1219360712142868488/5ldrsx1P_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1048815610778533889/1538894303",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1319323476801585200,
+			"quoted_status_id_str": "1319323476801585152",
+			"quoted_status_permalink": {
+				"url": "https://t.co/RLOSeVGP5q",
+				"expanded": "https://twitter.com/Teacher_Luke_UK/status/1319323476801585152",
+				"display": "twitter.com/Teacher_Luke_U…"
+			},
+			"retweet_count": 1,
+			"favorite_count": 15,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is a neat solution to managing virtual parent evenings. https://t.co/ysR8NjTBUm"
+	},
+	{
 		"created_at": "Mon Nov 02 21:43:27 +0000 2020",
 		"id": 1323380224470339600,
 		"id_str": "1323380224470339584",
