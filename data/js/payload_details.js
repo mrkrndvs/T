2bc4f0e76a3105d4a85d6fac2361f04e094176cf
Aaron Davis <mrkrndvs@gmail.com>
2020-12-24 09:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32013,
-	"created_at": "Wed Dec 23 12:14:59 +0000 2020",
+	"tweets": 32014,
+	"created_at": "Wed Dec 23 22:14:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Wed Dec 23 21:28:06 +0000 2020",
+		"id": 1341858142724866000,
+		"id_str": "1341858142724866048",
+		"full_text": "@edublogs Thank you, I assume you are talking about this https://t.co/1pULne9NuK. One aspect I think is overlooked in this change is the great support from the @edublogs team. As @kathleen_morris recently captured https://t.co/L9mX8F0VM0 it is still fair to say there is life after Global2",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			289
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "edublogs",
+					"name": "edublogs",
+					"id": 14091545,
+					"id_str": "14091545",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "edublogs",
+					"name": "edublogs",
+					"id": 14091545,
+					"id_str": "14091545",
+					"indices": [
+						160,
+						169
+					]
+				},
+				{
+					"screen_name": "kathleen_morris",
+					"name": "Kathleen Morris 🇦🇺",
+					"id": 23413264,
+					"id_str": "23413264",
+					"indices": [
+						179,
+						195
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1pULne9NuK",
+					"expanded_url": "https://help.edublogs.org/bulk-media-downloader-plugin/",
+					"display_url": "help.edublogs.org/bulk-media-dow…",
+					"indices": [
+						57,
+						80
+					]
+				},
+				{
+					"url": "https://t.co/L9mX8F0VM0",
+					"expanded_url": "https://www.theedublogger.com/school-library-websites-features-examples/",
+					"display_url": "theedublogger.com/school-library…",
+					"indices": [
+						214,
+						237
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1341852979771662300,
+		"in_reply_to_status_id_str": "1341852979771662343",
+		"in_reply_to_user_id": 14091545,
+		"in_reply_to_user_id_str": "14091545",
+		"in_reply_to_screen_name": "edublogs",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4189,
+			"friends_count": 989,
+			"listed_count": 359,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4916,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28348,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@edublogs Thank you, I assume you are talking about this https://t.co/1pULne9NuK. One aspect I think is overlooked in this change is the great support from the @edublogs team. As @kathleen_morris recently captured https://t.co/L9mX8F0VM0 it is still fair to say there is life after Global2"
+	},
+	{
 		"created_at": "Wed Dec 23 11:19:17 +0000 2020",
 		"id": 1341704930092929000,
 		"id_str": "1341704930092929026",
