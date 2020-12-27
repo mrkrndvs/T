3f08d43df4b994cff8c5e3191f9ed31175a51f3d
Aaron Davis <mrkrndvs@gmail.com>
2020-12-27 18:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32015,
-	"created_at": "Sat Dec 26 11:14:58 +0000 2020",
+	"tweets": 32016,
+	"created_at": "Sun Dec 27 07:14:59 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Sun Dec 27 06:36:01 +0000 2020",
+		"id": 1343083193671413800,
+		"id_str": "1343083193671413762",
+		"full_text": "Spent time in backyard today listening to music. Started down a rabbit hole w/ @EmpressOf, then @hernameisBANKS  &amp; realising BJ Burton connection, moved onto @SylvanEsso before jumping to @matthewherbert. Although was my physical space, left thinking of the shared space of sound",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EmpressOf",
+					"name": "Tia Empress Of",
+					"id": 580318221,
+					"id_str": "580318221",
+					"indices": [
+						79,
+						89
+					]
+				},
+				{
+					"screen_name": "hernameisBANKS",
+					"name": "BANKS",
+					"id": 1018579128,
+					"id_str": "1018579128",
+					"indices": [
+						96,
+						111
+					]
+				},
+				{
+					"screen_name": "SylvanEsso",
+					"name": "Sylvan Esso",
+					"id": 1265274192,
+					"id_str": "1265274192",
+					"indices": [
+						162,
+						173
+					]
+				},
+				{
+					"screen_name": "matthewherbert",
+					"name": "matthew herbert",
+					"id": 25862910,
+					"id_str": "25862910",
+					"indices": [
+						192,
+						207
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4186,
+			"friends_count": 990,
+			"listed_count": 357,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4916,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28350,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Spent time in backyard today listening to music. Started down a rabbit hole w/ @EmpressOf, then @hernameisBANKS  &amp; realising BJ Burton connection, moved onto @SylvanEsso before jumping to @matthewherbert. Although was my physical space, left thinking of the shared space of sound"
+	},
+	{
 		"created_at": "Sat Dec 26 11:07:50 +0000 2020",
 		"id": 1342789211250860000,
 		"id_str": "1342789211250860032",
