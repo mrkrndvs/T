510d0b9f742a0cb1f1860bfe76e24eda7a9b349f
Aaron Davis <mrkrndvs@gmail.com>
2021-01-01 21:15:00 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32026,
-	"created_at": "Thu Dec 31 23:14:59 +0000 2020",
+	"tweets": 32027,
+	"created_at": "Fri Jan 01 10:14:59 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_01.js
@@ -0,0 +1,410 @@
+Grailbird.data.tweets_2021_01 = 
+[
+	{
+		"created_at": "Fri Jan 01 10:04:19 +0000 2021",
+		"id": 1344947554723582000,
+		"id_str": "1344947554723581953",
+		"full_text": "I feel the #WarOn2020 video captures the frustration associated with changing one word and the hope that this satisfies the tension raised within the Uluru Statement from the Heart. https://t.co/jumNtoDwif",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WarOn2020",
+					"indices": [
+						11,
+						21
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jumNtoDwif",
+					"expanded_url": "https://twitter.com/chaser/status/1344846089959088128",
+					"display_url": "twitter.com/chaser/status/…",
+					"indices": [
+						182,
+						205
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4185,
+			"friends_count": 989,
+			"listed_count": 357,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28361,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1344846089959088000,
+		"quoted_status_id_str": "1344846089959088128",
+		"quoted_status_permalink": {
+			"url": "https://t.co/jumNtoDwif",
+			"expanded": "https://twitter.com/chaser/status/1344846089959088128",
+			"display": "twitter.com/chaser/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jan 01 03:21:08 +0000 2021",
+			"id": 1344846089959088000,
+			"id_str": "1344846089959088128",
+			"full_text": "In the spirit of Scott Morrison changing one word in the national anthem, here’s a special way to celebrate that momentous event. https://t.co/par2OayIcJ",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				153
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1339421048928800800,
+						"id_str": "1339421048928800768",
+						"indices": [
+							130,
+							153
+						],
+						"media_url": "http://pbs.twimg.com/media/EpaVn5vVQAAbSd5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EpaVn5vVQAAbSd5.jpg",
+						"url": "https://t.co/par2OayIcJ",
+						"display_url": "pic.twitter.com/par2OayIcJ",
+						"expanded_url": "https://twitter.com/chaser/status/1339422475377987587/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1339422475377987600,
+						"source_status_id_str": "1339422475377987587",
+						"source_user_id": 38612007,
+						"source_user_id_str": "38612007"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1339421048928800800,
+						"id_str": "1339421048928800768",
+						"indices": [
+							130,
+							153
+						],
+						"media_url": "http://pbs.twimg.com/media/EpaVn5vVQAAbSd5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EpaVn5vVQAAbSd5.jpg",
+						"url": "https://t.co/par2OayIcJ",
+						"display_url": "pic.twitter.com/par2OayIcJ",
+						"expanded_url": "https://twitter.com/chaser/status/1339422475377987587/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1339422475377987600,
+						"source_status_id_str": "1339422475377987587",
+						"source_user_id": 38612007,
+						"source_user_id_str": "38612007",
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 172880,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1339421048928800768/pl/PCEyitZx_0SxGML9.m3u8?tag=13"
+								},
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1339421048928800768/vid/720x720/loC_xg0nRDSev1dw.mp4?tag=13"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1339421048928800768/vid/320x320/M_DJRA0aUj18lZms.mp4?tag=13"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1339421048928800768/vid/480x480/MIV06_vfyH9-8wm6.mp4?tag=13"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "Let's celebrate Symbolic Gesture Day!",
+							"description": "The day to pat yourself on the back for doing absolutely nothing",
+							"embeddable": true,
+							"monetizable": false,
+							"source_user": {
+								"id": 38612007,
+								"id_str": "38612007",
+								"name": "The Chaser",
+								"screen_name": "chaser",
+								"location": "Sydney",
+								"description": "Drain on the taxpayer since 1999",
+								"url": "https://t.co/bN7Nlw0u0Y",
+								"entities": {
+									"url": {
+										"urls": [
+											{
+												"url": "https://t.co/bN7Nlw0u0Y",
+												"expanded_url": "http://www.chaser.com.au",
+												"display_url": "chaser.com.au",
+												"indices": [
+													0,
+													23
+												]
+											}
+										]
+									},
+									"description": {
+										"urls": []
+									}
+								},
+								"protected": false,
+								"followers_count": 136220,
+								"friends_count": 163,
+								"listed_count": 970,
+								"created_at": "Fri May 08 05:12:41 +0000 2009",
+								"favourites_count": 4057,
+								"utc_offset": null,
+								"time_zone": null,
+								"geo_enabled": true,
+								"verified": false,
+								"statuses_count": 4615,
+								"lang": null,
+								"contributors_enabled": false,
+								"is_translator": false,
+								"is_translation_enabled": false,
+								"profile_background_color": "DBE9ED",
+								"profile_background_image_url": "http://abs.twimg.com/images/themes/theme20/bg.png",
+								"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme20/bg.png",
+								"profile_background_tile": true,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/1328357739995615234/l3rpIuhi_normal.jpg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/1328357739995615234/l3rpIuhi_normal.jpg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/38612007/1572220216",
+								"profile_link_color": "D71920",
+								"profile_sidebar_border_color": "FFFFFF",
+								"profile_sidebar_fill_color": "E6F6F9",
+								"profile_text_color": "333333",
+								"profile_use_background_image": true,
+								"has_extended_profile": false,
+								"default_profile": false,
+								"default_profile_image": false,
+								"following": false,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "none"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 38612007,
+				"id_str": "38612007",
+				"name": "The Chaser",
+				"screen_name": "chaser",
+				"location": "Sydney",
+				"description": "Drain on the taxpayer since 1999",
+				"url": "https://t.co/bN7Nlw0u0Y",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bN7Nlw0u0Y",
+								"expanded_url": "http://www.chaser.com.au",
+								"display_url": "chaser.com.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 136220,
+				"friends_count": 163,
+				"listed_count": 970,
+				"created_at": "Fri May 08 05:12:41 +0000 2009",
+				"favourites_count": 4057,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4615,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "DBE9ED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme20/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme20/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1328357739995615234/l3rpIuhi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1328357739995615234/l3rpIuhi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/38612007/1572220216",
+				"profile_link_color": "D71920",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E6F6F9",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 613,
+			"favorite_count": 1775,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I feel the #WarOn2020 video captures the frustration associated with changing one word and the hope that this satisfies the tension raised within the Uluru Statement from the Heart. https://t.co/jumNtoDwif"
+	}
+]
