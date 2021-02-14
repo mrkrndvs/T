79e5af2c5efd2bdf56ec54328ffc8f1e95010cc3
Aaron Davis <mrkrndvs@gmail.com>
2021-02-14 22:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32049,
-	"created_at": "Fri Feb 12 12:14:58 +0000 2021",
+	"tweets": 32050,
+	"created_at": "Sun Feb 14 11:14:58 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Sun Feb 14 10:50:06 +0000 2021",
+		"id": 1360904143662059500,
+		"id_str": "1360904143662059520",
+		"full_text": "@ShortFormErnie @readtedium Ernie, here I was thinking that email was our biggest point of concern, clearly remote desktop access is up there too. Maybe this explain why Apple make you complete so many steps to connect?",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			219
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ShortFormErnie",
+					"name": "Ernie Smith",
+					"id": 31586980,
+					"id_str": "31586980",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "readtedium",
+					"name": "Tedium",
+					"id": 2903269822,
+					"id_str": "2903269822",
+					"indices": [
+						16,
+						27
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1360279959013634000,
+		"in_reply_to_status_id_str": "1360279959013634050",
+		"in_reply_to_user_id": 31586980,
+		"in_reply_to_user_id_str": "31586980",
+		"in_reply_to_screen_name": "ShortFormErnie",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4172,
+			"friends_count": 990,
+			"listed_count": 358,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4837,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28301,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ShortFormErnie @readtedium Ernie, here I was thinking that email was our biggest point of concern, clearly remote desktop access is up there too. Maybe this explain why Apple make you complete so many steps to connect?"
+	},
+	{
 		"created_at": "Fri Feb 12 11:50:54 +0000 2021",
 		"id": 1360194668496105500,
 		"id_str": "1360194668496105473",
