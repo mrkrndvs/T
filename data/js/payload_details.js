03d73cf98115fd3090a12a24d205848dd306814d
Aaron Davis <mrkrndvs@gmail.com>
2021-04-11 11:14:58 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32058,
-	"created_at": "Tue Apr 06 07:14:58 +0000 2021",
+	"tweets": 32059,
+	"created_at": "Sun Apr 11 01:14:58 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sun Apr 11 00:54:41 +0000 2021",
+		"id": 1381048019559227400,
+		"id_str": "1381048019559227399",
+		"full_text": "This reminds me of @JohnGCI discussion of 'listening to help the other person understand':\nhttps://t.co/yvWWFjMs41 https://t.co/M0zULB7pyM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JohnGCI",
+					"name": "John Campbell",
+					"id": 1368003330,
+					"id_str": "1368003330",
+					"indices": [
+						19,
+						27
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/yvWWFjMs41",
+					"expanded_url": "https://www.growthcoaching.com.au/articles-new/thanks-for-listening",
+					"display_url": "growthcoaching.com.au/articles-new/t…",
+					"indices": [
+						91,
+						114
+					]
+				},
+				{
+					"url": "https://t.co/M0zULB7pyM",
+					"expanded_url": "https://twitter.com/lithub/status/1369339792165662720",
+					"display_url": "twitter.com/lithub/status/…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4159,
+			"friends_count": 988,
+			"listed_count": 357,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4833,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28306,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1369339792165662700,
+		"quoted_status_id_str": "1369339792165662720",
+		"quoted_status_permalink": {
+			"url": "https://t.co/M0zULB7pyM",
+			"expanded": "https://twitter.com/lithub/status/1369339792165662720",
+			"display": "twitter.com/lithub/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Mar 09 17:30:22 +0000 2021",
+			"id": 1369339792165662700,
+			"id_str": "1369339792165662720",
+			"full_text": "\"To attend means not to seek, but to wait; not to concentrate, but instead to dilate our minds. We do not gain insights, Weil claims, by going in search of them, but instead by waiting for them.\" https://t.co/s3OHR3eTvV",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				219
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/s3OHR3eTvV",
+						"expanded_url": "https://lithub.com/simone-weils-radical-conception-of-attention/",
+						"display_url": "lithub.com/simone-weils-r…",
+						"indices": [
+							196,
+							219
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2903979971,
+				"id_str": "2903979971",
+				"name": "Literary Hub",
+				"screen_name": "lithub",
+				"location": "",
+				"description": "A daily literary website highlighting the best in contemporary fiction, nonfiction, and criticism.",
+				"url": "http://t.co/cnxp07z9xV",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/cnxp07z9xV",
+								"expanded_url": "http://www.lithub.com",
+								"display_url": "lithub.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 257813,
+				"friends_count": 988,
+				"listed_count": 3409,
+				"created_at": "Wed Dec 03 16:56:53 +0000 2014",
+				"favourites_count": 51586,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 41108,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/719305894534586368/xAlNwL1E_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/719305894534586368/xAlNwL1E_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2903979971/1423147382",
+				"profile_link_color": "8D1919",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 38,
+			"favorite_count": 113,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This reminds me of @JohnGCI discussion of 'listening to help the other person understand':\nhttps://t.co/yvWWFjMs41 https://t.co/M0zULB7pyM"
+	},
+	{
 		"created_at": "Tue Apr 06 07:11:47 +0000 2021",
 		"id": 1379330980624924700,
 		"id_str": "1379330980624924673",
