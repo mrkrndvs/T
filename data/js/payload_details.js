6722d6b6b02655b5d6b5357701ccc59eb84b20e6
Aaron Davis <mrkrndvs@gmail.com>
2021-06-03 21:14:58 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32073,
-	"created_at": "Fri May 28 12:14:58 +0000 2021",
+	"tweets": 32074,
+	"created_at": "Thu Jun 03 11:14:58 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,239 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Thu Jun 03 11:13:03 +0000 2021",
+		"id": 1400410192903241700,
+		"id_str": "1400410192903241728",
+		"full_text": "@JoelBSperanza @JoelBSperanza in many ways this reminds me of my experience with Disciplined Collaboration https://t.co/EzV6u2RwpI HT @AlmaHarris1 @MichelleSJones1 https://t.co/hqAfTvQGYT",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			163
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JoelBSperanza",
+					"name": "Joel Speranza",
+					"id": 469805226,
+					"id_str": "469805226",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "JoelBSperanza",
+					"name": "Joel Speranza",
+					"id": 469805226,
+					"id_str": "469805226",
+					"indices": [
+						15,
+						29
+					]
+				},
+				{
+					"screen_name": "AlmaHarris1",
+					"name": "Professor Alma Harris",
+					"id": 342121975,
+					"id_str": "342121975",
+					"indices": [
+						134,
+						146
+					]
+				},
+				{
+					"screen_name": "MichelleSJones1",
+					"name": "Dr Michelle Jones",
+					"id": 2555784314,
+					"id_str": "2555784314",
+					"indices": [
+						147,
+						163
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/EzV6u2RwpI",
+					"expanded_url": "https://readwriterespond.com/2015/06/disciplined-collaboration-allowing-freedom-within-form-finding-common-ground/",
+					"display_url": "readwriterespond.com/2015/06/discip…",
+					"indices": [
+						107,
+						130
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1400409890363822000,
+					"id_str": "1400409890363822081",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/media/E29BYCcUUAEIt9B.png",
+					"media_url_https": "https://pbs.twimg.com/media/E29BYCcUUAEIt9B.png",
+					"url": "https://t.co/hqAfTvQGYT",
+					"display_url": "pic.twitter.com/hqAfTvQGYT",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1400410192903241728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 456,
+							"h": 229,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 456,
+							"h": 229,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 456,
+							"h": 229,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1400409890363822000,
+					"id_str": "1400409890363822081",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/media/E29BYCcUUAEIt9B.png",
+					"media_url_https": "https://pbs.twimg.com/media/E29BYCcUUAEIt9B.png",
+					"url": "https://t.co/hqAfTvQGYT",
+					"display_url": "pic.twitter.com/hqAfTvQGYT",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1400410192903241728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 456,
+							"h": 229,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 456,
+							"h": 229,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 456,
+							"h": 229,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1397085388733239300,
+		"in_reply_to_status_id_str": "1397085388733239297",
+		"in_reply_to_user_id": 469805226,
+		"in_reply_to_user_id_str": "469805226",
+		"in_reply_to_screen_name": "JoelBSperanza",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4133,
+			"friends_count": 986,
+			"listed_count": 358,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4830,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28318,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@JoelBSperanza @JoelBSperanza in many ways this reminds me of my experience with Disciplined Collaboration https://t.co/EzV6u2RwpI HT @AlmaHarris1 @MichelleSJones1 https://t.co/hqAfTvQGYT"
+	}
+]
