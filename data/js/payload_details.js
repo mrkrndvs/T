bbfd6f4eb9cd505d9b73c4c1217eee1a09abaf37
Aaron Davis <mrkrndvs@gmail.com>
2021-07-29 22:14:58 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32089,
-	"created_at": "Mon Jul 26 12:14:58 +0000 2021",
+	"tweets": 32090,
+	"created_at": "Thu Jul 29 12:14:58 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 29 12:00:06 +0000 2021",
+		"id": 1419840971244675000,
+		"id_str": "1420715757470507014",
+		"full_text": "We are in the final week of collecting children and young people's perspectives on their lives as our first phase to inform @alannahmadeline's  Strategic Plan. \nIf you have a class, a team, a child at home. We'd love their input. Please share... https://t.co/uAkCZagdaV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "alannahmadeline",
+					"name": "Alannah Madeline Fdn",
+					"id": 111449574,
+					"id_str": "111449574",
+					"indices": [
+						124,
+						140
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uAkCZagdaV",
+					"expanded_url": "https://form.jotform.com/211740597263054",
+					"display_url": "form.jotform.com/211740597263054",
+					"indices": [
+						246,
+						269
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16051268,
+			"id_str": "16051268",
+			"name": "Dan Donahoo",
+			"screen_name": "ddonahoo",
+			"location": "Australia",
+			"description": "I'm just a guy. Standing in front of the internet. Wondering how we got here, exactly.",
+			"url": "https://t.co/ojHz4JRPvV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ojHz4JRPvV",
+							"expanded_url": "http://www.projectsynthesis.com.au",
+							"display_url": "projectsynthesis.com.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3323,
+			"friends_count": 416,
+			"listed_count": 232,
+			"created_at": "Sat Aug 30 03:48:25 +0000 2008",
+			"favourites_count": 4933,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 29552,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1044698811673661440/rr5_HuIC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1044698811673661440/rr5_HuIC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16051268/1502768450",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are in the final week of collecting children and young people's perspectives on their lives as our first phase to inform @alannahmadeline's  Strategic Plan. \nIf you have a class, a team, a child at home. We'd love their input. Please share... https://t.co/uAkCZagdaV"
+	},
+	{
 		"created_at": "Mon Jul 26 11:41:00 +0000 2021",
 		"id": 1419623787012702200,
 		"id_str": "1419623787012702210",
