90cd344949701559e23399efbb2f281208c12c0c
Aaron Davis <mrkrndvs@gmail.com>
2021-08-06 22:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32093,
-	"created_at": "Sun Aug 01 07:15:00 +0000 2021",
+	"tweets": 32094,
+	"created_at": "Fri Aug 06 12:14:58 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Fri Aug 06 11:15:14 +0000 2021",
+		"id": 1423603568838316000,
+		"id_str": "1423603568838316035",
+		"full_text": "@KatJoss @normanswan @teegstar @will_ock I concur Kathryn. Whether right or wrong, Coronacast has become a part of my morning routine. I was particularly interested in Norman’s take on Victoria’s move from donuts to lockdown. I guess life goes on.",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			247
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KatJoss",
+					"name": "Kathryn",
+					"id": 38586120,
+					"id_str": "38586120",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "normanswan",
+					"name": "Norman Swan",
+					"id": 246178914,
+					"id_str": "246178914",
+					"indices": [
+						9,
+						20
+					]
+				},
+				{
+					"screen_name": "teegstar",
+					"name": "Tegan Taylor",
+					"id": 14722572,
+					"id_str": "14722572",
+					"indices": [
+						21,
+						30
+					]
+				},
+				{
+					"screen_name": "will_ock",
+					"name": "Will Ockenden",
+					"id": 18265545,
+					"id_str": "18265545",
+					"indices": [
+						31,
+						40
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": 1423409860771602400,
+		"in_reply_to_status_id_str": "1423409860771602435",
+		"in_reply_to_user_id": 38586120,
+		"in_reply_to_user_id_str": "38586120",
+		"in_reply_to_screen_name": "KatJoss",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4122,
+			"friends_count": 986,
+			"listed_count": 0,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4828,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28321,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@KatJoss @normanswan @teegstar @will_ock I concur Kathryn. Whether right or wrong, Coronacast has become a part of my morning routine. I was particularly interested in Norman’s take on Victoria’s move from donuts to lockdown. I guess life goes on."
+	},
+	{
 		"created_at": "Sun Aug 01 06:59:31 +0000 2021",
 		"id": 1421727276698198000,
 		"id_str": "1421727276698198017",
