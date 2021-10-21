1ad44bef4d8b840f137543b45431fa644a1c83d3
Aaron Davis <mrkrndvs@gmail.com>
2021-10-21 21:14:58 +1100

    Added 1 tweets

--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 21 10:02:01 +0000 2021",
+		"id": 1450217334602301400,
+		"id_str": "1451126618332614660",
+		"full_text": "With Global 2 blogs now defunct, what are other #vicpln teachers using for blogging in/out of schools? #educhat #edublogging",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "vicpln",
+					"indices": [
+						48,
+						55
+					]
+				},
+				{
+					"text": "educhat",
+					"indices": [
+						103,
+						111
+					]
+				},
+				{
+					"text": "edublogging",
+					"indices": [
+						112,
+						124
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21286176,
+			"id_str": "21286176",
+			"name": "Rick Kayler-Thomson",
+			"screen_name": "rakt",
+			"location": "Bellarine Peninsula, Australia",
+			"description": "A primary STEM teacher on the Bellarine Peninsula, Australia. 1 of '2 Regular Teachers' podcast. Bellarine resident. All views are my own.",
+			"url": "https://t.co/nJLvK0r7mR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nJLvK0r7mR",
+							"expanded_url": "http://2regularteachers.global2.vic.edu.au/",
+							"display_url": "2regularteachers.global2.vic.edu.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1251,
+			"friends_count": 535,
+			"listed_count": 48,
+			"created_at": "Thu Feb 19 09:37:33 +0000 2009",
+			"favourites_count": 1124,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8087,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1128723646/jan09_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1128723646/jan09_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "With Global 2 blogs now defunct, what are other #vicpln teachers using for blogging in/out of schools? #educhat #edublogging"
+	},
+	{
 		"created_at": "Sun Oct 17 03:51:20 +0000 2021",
 		"id": 1449583784337215500,
 		"id_str": "1449583784337215488",
