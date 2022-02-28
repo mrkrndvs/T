7d9b73ae66d40841b917dcdd632d9c02394f1818
Aaron Davis <mrkrndvs@gmail.com>
2022-02-28 21:14:59 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32140,
-	"created_at": "Sun Feb 20 11:14:58 +0000 2022",
+	"tweets": 32141,
+	"created_at": "Mon Feb 28 10:14:58 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,10 +1,127 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 28 10:02:34 +0000 2022",
+		"id": 1498237180665380900,
+		"id_str": "1498237180665380864",
+		"full_text": "@taradcat Good luck for the future Deb. I will always treasure our experiences with the Melton Network and the ATC21s project.",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "taradcat",
+					"name": "Deb Hicks",
+					"id": 14152138,
+					"id_str": "14152138",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": 1498165517592395800,
+		"in_reply_to_status_id_str": "1498165517592395776",
+		"in_reply_to_user_id": 14152138,
+		"in_reply_to_user_id_str": "14152138",
+		"in_reply_to_screen_name": "taradcat",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4105,
+			"friends_count": 981,
+			"listed_count": 0,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4813,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28359,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@taradcat Good luck for the future Deb. I will always treasure our experiences with the Melton Network and the ATC21s project."
+	},
+	{
 		"created_at": "Sun Feb 20 10:27:05 +0000 2022",
 		"id": 1495145301728186400,
 		"id_str": "1495344249864593412",
-		"full_text": "I’ve been working on this #AFLW long read on #concussion for a while. Massive thanks to Macca for the courage to share her story, manager ⁦@CrossCodersCo⁩, ⁦@SMcleod_PT⁩, ⁦⁦@PinkConcussions⁩ and ⁦@amandashalala⁩ for edits! 💪👏 https://t.co/hdiJX4XeVB",
+		"full_text": "I’ve been working on this #AFLW long read on #concussion for a while. Massive thanks to Macca for the courage to share her story, manager @CrossCodersCo, @SMcleod_PT, @PinkConcussions and @amandashalala for edits! 💪👏 https://t.co/hdiJX4XeVB",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -161,7 +278,7 @@ Grailbird.data.tweets_2022_02 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "I’ve been working on this #AFLW long read on #concussion for a while. Massive thanks to Macca for the courage to share her story, manager ⁦@CrossCodersCo⁩, ⁦@SMcleod_PT⁩, ⁦⁦@PinkConcussions⁩ and ⁦@amandashalala⁩ for edits! 💪👏 https://t.co/hdiJX4XeVB"
+		"text": "I’ve been working on this #AFLW long read on #concussion for a while. Massive thanks to Macca for the courage to share her story, manager @CrossCodersCo, @SMcleod_PT, @PinkConcussions and @amandashalala for edits! 💪👏 https://t.co/hdiJX4XeVB"
 	},
 	{
 		"created_at": "Fri Feb 18 22:39:54 +0000 2022",
