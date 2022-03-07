d680511c099b46c2b10db7bea860ac319650e1da
Aaron Davis <mrkrndvs@gmail.com>
2022-03-07 23:14:58 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32143,
-	"created_at": "Sat Mar 05 06:14:58 +0000 2022",
+	"tweets": 32144,
+	"created_at": "Mon Mar 07 12:14:57 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 07 11:56:33 +0000 2022",
+		"id": 1500802579634454500,
+		"id_str": "1500802579634454528",
+		"full_text": "The We Are Open Co-op have collected together their various resources in one place, whether it be templates, online courses or episodes of the podcast. https://t.co/bu9KYx39Y8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bu9KYx39Y8",
+					"expanded_url": "https://twitter.com/WeAreOpenCoop/status/1498620302095441920",
+					"display_url": "twitter.com/WeAreOpenCoop/…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4106,
+			"friends_count": 981,
+			"listed_count": 352,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4814,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28363,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1498620302095442000,
+		"quoted_status_id_str": "1498620302095441920",
+		"quoted_status_permalink": {
+			"url": "https://t.co/bu9KYx39Y8",
+			"expanded": "https://twitter.com/WeAreOpenCoop/status/1498620302095441920",
+			"display": "twitter.com/WeAreOpenCoop/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Mar 01 11:24:57 +0000 2022",
+			"id": 1498620302095442000,
+			"id_str": "1498620302095441920",
+			"full_text": "🥳 LAUNCH DAY! 🥳 Led by our awesome intern @AOstsee, we've created a 'Learn with WAO' website to share free resources and help charities, non-profits, and impact-focused organisations https://t.co/s8ZhQNgQXb",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				206
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "AOstsee",
+						"name": "Anne Hilliger",
+						"id": 1145990535154491400,
+						"id_str": "1145990535154491392",
+						"indices": [
+							42,
+							50
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/s8ZhQNgQXb",
+						"expanded_url": "https://blog.weareopen.coop/learn-with-we-are-open-co-op-8a4dd9f31874",
+						"display_url": "blog.weareopen.coop/learn-with-we-…",
+						"indices": [
+							183,
+							206
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 722443103613939700,
+				"id_str": "722443103613939713",
+				"name": "We Are Open Co-op",
+				"screen_name": "WeAreOpenCoop",
+				"location": "",
+				"description": "Making sense of complex problems while spreading the benefits of open. Members: @dajbelshaw @bevangelist @epilepticrabbit @bryanmmathers",
+				"url": "https://t.co/FZsUz9Hwf4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/FZsUz9Hwf4",
+								"expanded_url": "http://weareopen.coop",
+								"display_url": "weareopen.coop",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1266,
+				"friends_count": 108,
+				"listed_count": 74,
+				"created_at": "Tue Apr 19 15:13:53 +0000 2016",
+				"favourites_count": 126,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 821,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1387796245092179972/ZhqTkvZ6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1387796245092179972/ZhqTkvZ6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/722443103613939713/1611577792",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 34,
+			"favorite_count": 33,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The We Are Open Co-op have collected together their various resources in one place, whether it be templates, online courses or episodes of the podcast. https://t.co/bu9KYx39Y8"
+	},
+	{
 		"created_at": "Sat Mar 05 05:32:39 +0000 2022",
 		"id": 1499981194066542600,
 		"id_str": "1499981194066542592",
