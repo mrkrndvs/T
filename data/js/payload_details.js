85dd3860f847706e7a94dcd4ee3a4372c062330b
Aaron Davis <mrkrndvs@gmail.com>
2022-03-20 13:14:58 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32145,
-	"created_at": "Tue Mar 08 09:14:58 +0000 2022",
+	"tweets": 32146,
+	"created_at": "Sun Mar 20 02:14:57 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sun Mar 20 01:34:01 +0000 2022",
+		"id": 1505280639863767000,
+		"id_str": "1505356957414486016",
+		"full_text": "I'm seeking some educators to help advise on resource development for a digital ethics project. I know the world of education is crazy right now. If you have a couple of hours to spare me - DM or email. Details: https://t.co/Ch76NvPCdi…… #aussieED #Teachers",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			257
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aussieED",
+					"indices": [
+						238,
+						247
+					]
+				},
+				{
+					"text": "Teachers",
+					"indices": [
+						248,
+						257
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ch76NvPCdi",
+					"expanded_url": "https://dropbox.com/s/n1kbnh6hpwv0wdd/%5BIRL%5D%20Seeking%20Teachers.pdf?dl=0",
+					"display_url": "dropbox.com/s/n1kbnh6hpwv0…",
+					"indices": [
+						212,
+						235
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16051268,
+			"id_str": "16051268",
+			"name": "Dan Donahoo",
+			"screen_name": "ddonahoo",
+			"location": "Australia",
+			"description": "I'm just a guy. Standing in front of the internet. Wondering how we got here, exactly.",
+			"url": "https://t.co/ojHz4JRPvV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ojHz4JRPvV",
+							"expanded_url": "http://www.projectsynthesis.com.au",
+							"display_url": "projectsynthesis.com.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3317,
+			"friends_count": 363,
+			"listed_count": 230,
+			"created_at": "Sat Aug 30 03:48:25 +0000 2008",
+			"favourites_count": 5720,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30037,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1044698811673661440/rr5_HuIC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1044698811673661440/rr5_HuIC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16051268/1502768450",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm seeking some educators to help advise on resource development for a digital ethics project. I know the world of education is crazy right now. If you have a couple of hours to spare me - DM or email. Details: https://t.co/Ch76NvPCdi…… #aussieED #Teachers"
+	},
+	{
 		"created_at": "Tue Mar 08 08:40:43 +0000 2022",
 		"id": 1501115685107286000,
 		"id_str": "1501115685107286020",
