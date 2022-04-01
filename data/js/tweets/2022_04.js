155a86e6337b54f8d1ac43005e328ad0dc4532b1
Aaron Davis <mrkrndvs@gmail.com>
2022-04-01 23:14:58 +1100

    Added 1 tweets

---
+++ data/js/tweets/2022_04.js
@@ -0,0 +1,110 @@
+Grailbird.data.tweets_2022_04 = 
+[
+	{
+		"created_at": "Fri Apr 01 11:17:52 +0000 2022",
+		"id": 1509697843799683000,
+		"id_str": "1509852540682502151",
+		"full_text": "Been a long while. Sorry. Q. Anyone know if an alternative for @intel s old Visual Ranking Tool??? It was brilliant for teachers/kids but appears to have vanished.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			163
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "intel",
+					"name": "Intel",
+					"id": 2803191,
+					"id_str": "2803191",
+					"indices": [
+						63,
+						69
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 196529471,
+			"id_str": "196529471",
+			"name": "Corrie Barclay",
+			"screen_name": "CorrieB",
+			"location": "Geelong, Vic, Australia",
+			"description": "Principal @ Newtown PS in Geelong.  ADE 13. GTA '11. Passionate about education, futuristic learning, & building capacity in others.",
+			"url": "https://t.co/QCnE1WmfFh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QCnE1WmfFh",
+							"expanded_url": "http://cbarclay.global2.vic.edu.au/",
+							"display_url": "cbarclay.global2.vic.edu.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3448,
+			"friends_count": 2669,
+			"listed_count": 226,
+			"created_at": "Wed Sep 29 09:55:08 +0000 2010",
+			"favourites_count": 4060,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12810,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/499135157983383553/pIcxvJf1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/499135157983383553/pIcxvJf1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/196529471/1403655081",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Been a long while. Sorry. Q. Anyone know if an alternative for @intel s old Visual Ranking Tool??? It was brilliant for teachers/kids but appears to have vanished."
+	}
+]
