948ac916b8586b2870e3fe19f38977c5a93e25b7
Aaron Davis <mrkrndvs@gmail.com>
2022-04-14 23:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32155,
-	"created_at": "Thu Apr 14 11:14:58 +0000 2022",
+	"tweets": 32156,
+	"created_at": "Thu Apr 14 13:14:58 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Thu Apr 14 13:01:25 +0000 2022",
+		"id": 1514589644092223500,
+		"id_str": "1514589644092223490",
+		"full_text": "@molly0xFFF has created @web3isgreat, a site collating examples of how Web3 is not going as well as suggested https://t.co/MwOve1VA4u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "molly0xFFF",
+					"name": "Molly White",
+					"id": 545445165,
+					"id_str": "545445165",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "web3isgreat",
+					"name": "web3 is going just great",
+					"id": 1477342011875381200,
+					"id_str": "1477342011875381251",
+					"indices": [
+						24,
+						36
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MwOve1VA4u",
+					"expanded_url": "https://twitter.com/funnymonkey/status/1514576506680492036",
+					"display_url": "twitter.com/funnymonkey/st…",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 545445165,
+		"in_reply_to_user_id_str": "545445165",
+		"in_reply_to_screen_name": "molly0xFFF",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4100,
+			"friends_count": 858,
+			"listed_count": 352,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4803,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28372,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1514576506680492000,
+		"quoted_status_id_str": "1514576506680492036",
+		"quoted_status_permalink": {
+			"url": "https://t.co/MwOve1VA4u",
+			"expanded": "https://twitter.com/funnymonkey/status/1514576506680492036",
+			"display": "twitter.com/funnymonkey/st…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Apr 14 12:09:13 +0000 2022",
+			"id": 1514576506680492000,
+			"id_str": "1514576506680492036",
+			"full_text": "I just discovered https://t.co/NJH82lQBtU from @molly0xFFF and I am both embarrassed it took me this long to find the site and overjoyed the site exists.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				153
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "molly0xFFF",
+						"name": "Molly White",
+						"id": 545445165,
+						"id_str": "545445165",
+						"indices": [
+							47,
+							58
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/NJH82lQBtU",
+						"expanded_url": "https://web3isgoinggreat.com/",
+						"display_url": "web3isgoinggreat.com",
+						"indices": [
+							18,
+							41
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 12127972,
+				"id_str": "12127972",
+				"name": "Bill Fitzgerald (he/him)",
+				"screen_name": "funnymonkey",
+				"location": "",
+				"description": "Personal Acc't. Speaking only for myself. Privacy, Misinformation, AdTech, Education, Open Source, Content, and Standards.  Education is a social justice issue.",
+				"url": "https://t.co/WRJHNB5fVy",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WRJHNB5fVy",
+								"expanded_url": "https://funnymonkey.com",
+								"display_url": "funnymonkey.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10316,
+				"friends_count": 6188,
+				"listed_count": 579,
+				"created_at": "Fri Jan 11 19:38:54 +0000 2008",
+				"favourites_count": 5815,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1253,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "59472F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/930647003779891200/WxAvzCJh_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/930647003779891200/WxAvzCJh_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/12127972/1398229690",
+				"profile_link_color": "52555C",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "061127",
+				"profile_text_color": "827972",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@molly0xFFF has created @web3isgreat, a site collating examples of how Web3 is not going as well as suggested https://t.co/MwOve1VA4u"
+	},
+	{
 		"created_at": "Thu Apr 14 10:48:04 +0000 2022",
 		"id": 1514556086715830300,
 		"id_str": "1514556086715830274",
