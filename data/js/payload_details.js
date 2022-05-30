44fecdccc6523c9628061d13beaa0c7c2bac26f8
Aaron Davis <mrkrndvs@gmail.com>
2022-05-30 22:14:58 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32171,
-	"created_at": "Tue May 24 12:14:58 +0000 2022",
+	"tweets": 32172,
+	"created_at": "Mon May 30 12:14:58 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 30 11:36:08 +0000 2022",
+		"id": 1531238024956485600,
+		"id_str": "1531238024956485634",
+		"full_text": "@stringer_andrea @AdamMGrant Andrea, this reminds me of @davecormier's post about 'care' as learning's first principle https://t.co/1nXALfOtG2 https://t.co/X5v3KeVtBS",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stringer_andrea",
+					"name": "Andrea Stringer 🇦🇺",
+					"id": 585586447,
+					"id_str": "585586447",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "AdamMGrant",
+					"name": "Adam Grant",
+					"id": 1059273780,
+					"id_str": "1059273780",
+					"indices": [
+						17,
+						28
+					]
+				},
+				{
+					"screen_name": "davecormier",
+					"name": "dave cormier",
+					"id": 789991,
+					"id_str": "789991",
+					"indices": [
+						56,
+						68
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1nXALfOtG2",
+					"expanded_url": "http://davecormier.com/edblog/2014/12/21/learnings-first-principle-the-most-important-thing-i-learned-this-year/",
+					"display_url": "davecormier.com/edblog/2014/12…",
+					"indices": [
+						119,
+						142
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1531237639139242000,
+					"id_str": "1531237639139241985",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/FUAMhRuaMAEeIsW.png",
+					"media_url_https": "https://pbs.twimg.com/media/FUAMhRuaMAEeIsW.png",
+					"url": "https://t.co/X5v3KeVtBS",
+					"display_url": "pic.twitter.com/X5v3KeVtBS",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1531238024956485634/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 328,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 328,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 328,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1531237639139242000,
+					"id_str": "1531237639139241985",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/FUAMhRuaMAEeIsW.png",
+					"media_url_https": "https://pbs.twimg.com/media/FUAMhRuaMAEeIsW.png",
+					"url": "https://t.co/X5v3KeVtBS",
+					"display_url": "pic.twitter.com/X5v3KeVtBS",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1531238024956485634/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 328,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 328,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 328,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1530127022923874300,
+		"in_reply_to_status_id_str": "1530127022923874305",
+		"in_reply_to_user_id": 585586447,
+		"in_reply_to_user_id_str": "585586447",
+		"in_reply_to_screen_name": "stringer_andrea",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4087,
+			"friends_count": 855,
+			"listed_count": 351,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4800,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28387,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@stringer_andrea @AdamMGrant Andrea, this reminds me of @davecormier's post about 'care' as learning's first principle https://t.co/1nXALfOtG2 https://t.co/X5v3KeVtBS"
+	},
+	{
 		"created_at": "Tue May 24 11:29:12 +0000 2022",
 		"id": 1529061952894664700,
 		"id_str": "1529061952894664704",
