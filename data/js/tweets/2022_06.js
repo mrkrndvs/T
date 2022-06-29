966131270bb9f93572f23e6aa8f846a790ef16d3
Aaron Davis <mrkrndvs@gmail.com>
2022-06-29 22:14:58 +1000

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,242 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 29 11:41:51 +0000 2022",
+		"id": 1542111098337632300,
+		"id_str": "1542111098337632256",
+		"full_text": "@thisgirl_writes @AffirmPress @fionathehardy @thisgirl_writes I really like the rich characters that @fionathehardy captures through her stories. I think you captures this clearly in your review https://t.co/X40KDgan57",
+		"truncated": false,
+		"display_text_range": [
+			45,
+			194
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thisgirl_writes",
+					"name": "Laura 🦋",
+					"id": 618900032,
+					"id_str": "618900032",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "AffirmPress",
+					"name": "Affirm Press",
+					"id": 66235127,
+					"id_str": "66235127",
+					"indices": [
+						17,
+						29
+					]
+				},
+				{
+					"screen_name": "fionathehardy",
+					"name": "fiona hardy",
+					"id": 208438518,
+					"id_str": "208438518",
+					"indices": [
+						30,
+						44
+					]
+				},
+				{
+					"screen_name": "thisgirl_writes",
+					"name": "Laura 🦋",
+					"id": 618900032,
+					"id_str": "618900032",
+					"indices": [
+						45,
+						61
+					]
+				},
+				{
+					"screen_name": "fionathehardy",
+					"name": "fiona hardy",
+					"id": 208438518,
+					"id_str": "208438518",
+					"indices": [
+						101,
+						115
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1542110205227704300,
+					"id_str": "1542110205227704321",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/FWatD6saIAEO-6s.png",
+					"media_url_https": "https://pbs.twimg.com/media/FWatD6saIAEO-6s.png",
+					"url": "https://t.co/X40KDgan57",
+					"display_url": "pic.twitter.com/X40KDgan57",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1542111098337632256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 514,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 514,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 514,
+							"h": 380,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1542110205227704300,
+					"id_str": "1542110205227704321",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/FWatD6saIAEO-6s.png",
+					"media_url_https": "https://pbs.twimg.com/media/FWatD6saIAEO-6s.png",
+					"url": "https://t.co/X40KDgan57",
+					"display_url": "pic.twitter.com/X40KDgan57",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1542111098337632256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 514,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 514,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 514,
+							"h": 380,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1533615013168091100,
+		"in_reply_to_status_id_str": "1533615013168091137",
+		"in_reply_to_user_id": 618900032,
+		"in_reply_to_user_id_str": "618900032",
+		"in_reply_to_screen_name": "thisgirl_writes",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4075,
+			"friends_count": 853,
+			"listed_count": 351,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4798,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28396,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@thisgirl_writes @AffirmPress @fionathehardy @thisgirl_writes I really like the rich characters that @fionathehardy captures through her stories. I think you captures this clearly in your review https://t.co/X40KDgan57"
+	},
+	{
 		"created_at": "Wed Jun 22 11:51:58 +0000 2022",
 		"id": 1539576928604397600,
 		"id_str": "1539576928604397568",
