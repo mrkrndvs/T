a560170c53c03068dc5fd79045ed7ffb097ab782
Aaron Davis <mrkrndvs@gmail.com>
2022-07-30 19:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32183,
-	"created_at": "Fri Jul 29 10:14:58 +0000 2022",
+	"tweets": 32184,
+	"created_at": "Sat Jul 30 09:14:58 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,381 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Sat Jul 30 08:28:46 +0000 2022",
+		"id": 1553296529611694000,
+		"id_str": "1553296529611694083",
+		"full_text": "Enjoyed listening to Tom Tilley's memoir. What I found interesting was the discussion of connection and community throughout. Although the church connections shriveled up, connection to Mudgee seemed to always stay strong. This felt as much to do with place as the people in it. https://t.co/juOj3vbZcK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/juOj3vbZcK",
+					"expanded_url": "https://twitter.com/TomTilley/status/1483198717197176835",
+					"display_url": "twitter.com/TomTilley/stat…",
+					"indices": [
+						279,
+						302
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4067,
+			"friends_count": 726,
+			"listed_count": 349,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4795,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28396,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1483198717197176800,
+		"quoted_status_id_str": "1483198717197176835",
+		"quoted_status_permalink": {
+			"url": "https://t.co/juOj3vbZcK",
+			"expanded": "https://twitter.com/TomTilley/status/1483198717197176835",
+			"display": "twitter.com/TomTilley/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jan 17 22:05:05 +0000 2022",
+			"id": 1483198717197176800,
+			"id_str": "1483198717197176835",
+			"full_text": "For the past two years I’ve been pouring my time and soul into a memoir about my childhood in a pentecostal sect. I’m hoping it inspires anyone who grew up questioning the life they inherited. Here’s the cover, pre-orders@ https://t.co/Int2JFRUJt \n@HarperCollinsAU @ReadABCBooks https://t.co/MTeoKR2hwN",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				278
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "HarperCollinsAU",
+						"name": "HarperCollins Aus",
+						"id": 23711301,
+						"id_str": "23711301",
+						"indices": [
+							248,
+							264
+						]
+					},
+					{
+						"screen_name": "ReadABCBooks",
+						"name": "ABC Books",
+						"id": 123162087,
+						"id_str": "123162087",
+						"indices": [
+							265,
+							278
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/Int2JFRUJt",
+						"expanded_url": "https://bit.ly/3nfkMNz",
+						"display_url": "bit.ly/3nfkMNz",
+						"indices": [
+							223,
+							246
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1483198691507060700,
+						"id_str": "1483198691507060736",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/media/FJVhWgKaQAAGJPr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FJVhWgKaQAAGJPr.jpg",
+						"url": "https://t.co/MTeoKR2hwN",
+						"display_url": "pic.twitter.com/MTeoKR2hwN",
+						"expanded_url": "https://twitter.com/TomTilley/status/1483198717197176835/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1152,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1483198691507060700,
+						"id_str": "1483198691507060736",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/media/FJVhWgKaQAAGJPr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FJVhWgKaQAAGJPr.jpg",
+						"url": "https://t.co/MTeoKR2hwN",
+						"display_url": "pic.twitter.com/MTeoKR2hwN",
+						"expanded_url": "https://twitter.com/TomTilley/status/1483198717197176835/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1152,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1483198691679019000,
+						"id_str": "1483198691679019008",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/media/FJVhWgzaIAAk9yp.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FJVhWgzaIAAk9yp.jpg",
+						"url": "https://t.co/MTeoKR2hwN",
+						"display_url": "pic.twitter.com/MTeoKR2hwN",
+						"expanded_url": "https://twitter.com/TomTilley/status/1483198717197176835/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20946600,
+				"id_str": "20946600",
+				"name": "Tom Tilley",
+				"screen_name": "TomTilley",
+				"location": "Sydney",
+				"description": "📖 SPEAKING IN TONGUES - out May via Harper Collins/ABC Books PRE-ORDER NOW! (mgt - https://t.co/eUPXHyKZan)",
+				"url": "https://t.co/DpWFYuhcwX",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/DpWFYuhcwX",
+								"expanded_url": "https://www.booktopia.com.au/speaking-in-tongues-tom-tilley/book/9780733341021.html",
+								"display_url": "booktopia.com.au/speaking-in-to…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/eUPXHyKZan",
+								"expanded_url": "http://thefordhamcompany.com.au",
+								"display_url": "thefordhamcompany.com.au",
+								"indices": [
+									83,
+									106
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 32608,
+				"friends_count": 4161,
+				"listed_count": 0,
+				"created_at": "Sun Feb 15 23:23:33 +0000 2009",
+				"favourites_count": 7775,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 5068,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1123827323122454529/_iC43wH4_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123827323122454529/_iC43wH4_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/20946600/1642457531",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 30,
+			"favorite_count": 240,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Enjoyed listening to Tom Tilley's memoir. What I found interesting was the discussion of connection and community throughout. Although the church connections shriveled up, connection to Mudgee seemed to always stay strong. This felt as much to do with place as the people in it. https://t.co/juOj3vbZcK"
+	},
+	{
 		"created_at": "Fri Jul 29 09:37:12 +0000 2022",
 		"id": 1552916113486655500,
 		"id_str": "1552951365949095936",
