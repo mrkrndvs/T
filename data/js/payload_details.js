b8e230c5b00275a4dab4a0b07ab9685e0a04f520
Aaron Davis <mrkrndvs@gmail.com>
2022-09-07 23:14:59 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32185,
-	"created_at": "Mon Aug 01 11:14:58 +0000 2022",
+	"tweets": 32186,
+	"created_at": "Wed Sep 07 13:14:58 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_09.js
@@ -0,0 +1,209 @@
+Grailbird.data.tweets_2022_09 = 
+[
+	{
+		"created_at": "Wed Sep 07 12:41:24 +0000 2022",
+		"id": 1567493235655999500,
+		"id_str": "1567493235655999489",
+		"full_text": "@cpaterso I think this is really interesting idea, the use of badges as stepping stones. My question though is what students do with these badges. For me, I am really intrigued by the idea of 'constellations'. https://t.co/Xj1ReGeDp1 https://t.co/hR2f0YZ7Ne",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			233
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cpaterso",
+					"name": "Cameron",
+					"id": 16181607,
+					"id_str": "16181607",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Xj1ReGeDp1",
+					"expanded_url": "https://readwriterespond.com/2016/12/comprehensive-guide-open-badges/",
+					"display_url": "readwriterespond.com/2016/12/compre…",
+					"indices": [
+						210,
+						233
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1567493081460805600,
+					"id_str": "1567493081460805637",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/FcDapxRakAUmfCy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcDapxRakAUmfCy.jpg",
+					"url": "https://t.co/hR2f0YZ7Ne",
+					"display_url": "pic.twitter.com/hR2f0YZ7Ne",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1567493235655999489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1802,
+							"h": 1144,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 762,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 432,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1567493081460805600,
+					"id_str": "1567493081460805637",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/FcDapxRakAUmfCy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcDapxRakAUmfCy.jpg",
+					"url": "https://t.co/hR2f0YZ7Ne",
+					"display_url": "pic.twitter.com/hR2f0YZ7Ne",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1567493235655999489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1802,
+							"h": 1144,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 762,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 432,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1564744872728600600,
+		"in_reply_to_status_id_str": "1564744872728600576",
+		"in_reply_to_user_id": 16181607,
+		"in_reply_to_user_id_str": "16181607",
+		"in_reply_to_screen_name": "cpaterso",
+		"user": {
+			"id": 372768752,
+			"id_str": "372768752",
+			"name": "Aaron Davis 🏘️",
+			"screen_name": "mrkrndvs",
+			"location": "Melbourne, Australia",
+			"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+			"url": "https://t.co/9kvcoZA3IC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9kvcoZA3IC",
+							"expanded_url": "https://collect.readwriterespond.com",
+							"display_url": "collect.readwriterespond.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uTlsCeAwJ7",
+							"expanded_url": "https://readwriterespond.com",
+							"display_url": "readwriterespond.com",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4061,
+			"friends_count": 726,
+			"listed_count": 349,
+			"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+			"favourites_count": 4790,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28391,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@cpaterso I think this is really interesting idea, the use of badges as stepping stones. My question though is what students do with these badges. For me, I am really intrigued by the idea of 'constellations'. https://t.co/Xj1ReGeDp1 https://t.co/hR2f0YZ7Ne"
+	}
+]
